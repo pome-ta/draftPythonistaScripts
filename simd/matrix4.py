4b931371f1a31689de6d33d1bec78044b4592b95
pome-ta <pometarou93@gmail.com>
2021-12-08 22:18:23 +0900

    transforms

--- simd/matrix4.py
+++ simd/matrix4.py
@@ -43,14 +43,62 @@ class Matrix4(ctypes.Union):
   [{values[12]:.4f}, {values[13]:.4f}, {values[14]:.4f}, {values[15]:.4f}]'''
     return mstr
 
-  def __init__(self, *args, **kw):
+  def __mul__(self, other):
+    if isinstance(other, self.__class__):
+      return Matrix4Multiply(self, other)
+    else:
+      raise NotImplementedError()
+
+  def __init__(self,
+               col_x=(1.0, 0.0, 0.0, 0.0),
+               col_y=(0.0, 1.0, 0.0, 0.0),
+               col_z=(0.0, 0.0, 1.0, 0.0),
+               col_w=(0.0, 0.0, 0.0, 1.0),
+               *args, **kw):
     super().__init__(*args, **kw)
-    cols = (
-      Vector4(1.0, 0.0, 0.0, 0.0),
-      Vector4(0.0, 1.0, 0.0, 0.0),
-      Vector4(0.0, 0.0, 1.0, 0.0),
-      Vector4(0.0, 0.0, 0.0, 1.0))
+    cols = (Vector4(*col_x), Vector4(*col_y), Vector4(*col_z), Vector4(*col_w))
     self.columns = cols
+
+
+def Matrix4Multiply(matrixLeft, matrixRight):
+  m = Matrix4()
+  m.m[0] = (matrixLeft.m[0] * matrixRight.m[0] + matrixLeft.m[4] * matrixRight.m[1]
+            + matrixLeft.m[8] * matrixRight.m[2] + matrixLeft.m[12] * matrixRight.m[3])
+  m.m[4] = (matrixLeft.m[0] * matrixRight.m[4] + matrixLeft.m[4] * matrixRight.m[5]
+            + matrixLeft.m[8] * matrixRight.m[6] + matrixLeft.m[12] * matrixRight.m[7])
+  m.m[8] = (matrixLeft.m[0] * matrixRight.m[8] + matrixLeft.m[4] * matrixRight.m[9]
+            + matrixLeft.m[8] * matrixRight.m[10] + matrixLeft.m[12] * matrixRight.m[11])
+  m.m[12] = (matrixLeft.m[0] * matrixRight.m[12] + matrixLeft.m[4] * matrixRight.m[13]
+             + matrixLeft.m[8] * matrixRight.m[14] + matrixLeft.m[12] * matrixRight.m[15])
+
+  m.m[1] = (matrixLeft.m[1] * matrixRight.m[0] + matrixLeft.m[5] * matrixRight.m[1]
+            + matrixLeft.m[9] * matrixRight.m[2] + matrixLeft.m[13] * matrixRight.m[3])
+  m.m[5] = (matrixLeft.m[1] * matrixRight.m[4] + matrixLeft.m[5] * matrixRight.m[5]
+            + matrixLeft.m[9] * matrixRight.m[6] + matrixLeft.m[13] * matrixRight.m[7])
+  m.m[9] = (matrixLeft.m[1] * matrixRight.m[8] + matrixLeft.m[5] * matrixRight.m[9]
+            + matrixLeft.m[9] * matrixRight.m[10] + matrixLeft.m[13] * matrixRight.m[11])
+  m.m[13] = (matrixLeft.m[1] * matrixRight.m[12] + matrixLeft.m[5] * matrixRight.m[13]
+             + matrixLeft.m[9] * matrixRight.m[14] + matrixLeft.m[13] * matrixRight.m[15])
+
+  m.m[2] = (matrixLeft.m[2] * matrixRight.m[0] + matrixLeft.m[6] * matrixRight.m[1]
+            + matrixLeft.m[10] * matrixRight.m[2] + matrixLeft.m[14] * matrixRight.m[3])
+  m.m[6] = (matrixLeft.m[2] * matrixRight.m[4] + matrixLeft.m[6] * matrixRight.m[5]
+            + matrixLeft.m[10] * matrixRight.m[6] + matrixLeft.m[14] * matrixRight.m[7])
+  m.m[10] = (matrixLeft.m[2] * matrixRight.m[8] + matrixLeft.m[6] * matrixRight.m[9]
+             + matrixLeft.m[10] * matrixRight.m[10] + matrixLeft.m[14] * matrixRight.m[11])
+  m.m[14] = (matrixLeft.m[2] * matrixRight.m[12] + matrixLeft.m[6] * matrixRight.m[13]
+             + matrixLeft.m[10] * matrixRight.m[14] + matrixLeft.m[14] * matrixRight.m[15])
+
+  m.m[3] = (matrixLeft.m[3] * matrixRight.m[0] + matrixLeft.m[7] * matrixRight.m[1]
+            + matrixLeft.m[11] * matrixRight.m[2] + matrixLeft.m[15] * matrixRight.m[3])
+  m.m[7] = (matrixLeft.m[3] * matrixRight.m[4] + matrixLeft.m[7] * matrixRight.m[5]
+            + matrixLeft.m[11] * matrixRight.m[6] + matrixLeft.m[15] * matrixRight.m[7])
+  m.m[11] = (matrixLeft.m[3] * matrixRight.m[8] + matrixLeft.m[7] * matrixRight.m[9]
+             + matrixLeft.m[11] * matrixRight.m[10] + matrixLeft.m[15] * matrixRight.m[11])
+  m.m[15] = (matrixLeft.m[3] * matrixRight.m[12] + matrixLeft.m[7] * matrixRight.m[13]
+             + matrixLeft.m[11] * matrixRight.m[14] + matrixLeft.m[15] * matrixRight.m[15])
+
+  return m
 
 
 if __name__ == '__main__':
---
+++ simd/transforms.py
@@ -0,0 +1,54 @@
+import math
+from vector3 import Vector3, Vector3Normalize
+from matrix4 import Matrix4
+
+
+def matrix4x4_translation(tx, ty, tz):
+  return Matrix4(
+    (1.0, 0.0, 0.0, 0.0),
+    (0.0, 1.0, 0.0, 0.0),
+    (0.0, 0.0, 1.0, 0.0),
+    (tx, ty, tz, 1.0)
+  )
+
+
+def matrix4x4_rotation(radians, axis):
+  # xxx: Vector3 をどこで検知するか？
+  if isinstance(axis, Vector3):
+    axis = Vector3Normalize(axis)
+  else:
+    axis = Vector3Normalize(Vector3(*axis))
+  ct = math.cos(radians)
+  st = math.sin(radians)
+  ci = 1 - ct
+  x = axis.x
+  y = axis.y
+  z = axis.z
+  return Matrix4(
+    (ct + x * x * ci, y * x * ci + z * st, z * x * ci - y * st, 0.0),
+    (x * y * ci - z * st, ct + y * y * ci, z * y * ci + x * st, 0.0),
+    (x * z * ci + y * st, y * z * ci - x * st, ct + z * z * ci, 0.0),
+    (0.0, 0.0, 0.0, 1.0)
+  )
+
+
+def matrix4x4_scale(sx, sy, sz):
+  return Matrix4(
+    (sx, 0.0, 0.0, 0.0),
+    (0.0, sy, 0.0, 0.0),
+    (0.0, 0.0, sz, 0.0),
+    (0.0, 0.0, 0.0, 1.0)
+  )
+
+
+if __name__ == '__main__':
+  testTransform = matrix4x4_translation(0.3275, 0.3, 0.3725)
+  print(f'testTransform: {testTransform}')
+  testScale = matrix4x4_scale(0.6, 0.6, 0.6)
+  print(f'testScale: {testScale}')
+  testRotation = matrix4x4_rotation(-0.3, (0.0, 1.0, 0.0))
+  print(f'testRotation: {testRotation}')
+  transform01 = matrix4x4_translation(0.3275, 0.3, 0.3725) * matrix4x4_rotation(-0.3, Vector3(0.0, 1.0, 0.0)) * matrix4x4_scale( 0.6, 0.6, 0.6)
+  print(f'transform01: {transform01}')
+  transform02 = matrix4x4_translation(0.0, 1.0, 0.0) * matrix4x4_scale(0.5, 1.98, 0.5)
+  print(f'transform02: {transform02}')
