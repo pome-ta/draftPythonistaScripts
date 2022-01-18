23f5aaefde981b6cc2020b6847ee318ef8184208
pome-ta <pome-ta@users.noreply.github.com>
2022-01-19 07:31:43 +0900

    WKWebView

--- sfSafariViewController/serverRun.py
+++ sfSafariViewController/serverRun.py
@@ -1,30 +1,13 @@
 import os
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from objc_util import ObjCClass, UIApplication, on_main_thread, nsurl
 
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-
+import pdbg
 
 os.chdir(os.path.join(os.path.dirname(__file__), 'public'))
 
-
-
-class RequestHandler(SimpleHTTPRequestHandler, object):
-  def print_info(self):
-    #self.log_message("%s %s\n%s", self.command, self.path, self.headers)
-    pass
-
-  def do_GET(self):
-    #self.print_info()
-    super(RequestHandler, self).do_GET()
-
-  def do_POST(self):
-    self.do_GET()
-
-
 httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
-
 
 SFSafariViewController = ObjCClass('SFSafariViewController')
 
@@ -37,18 +20,21 @@ def open_in_safari_vc(url):
 
   app = UIApplication.sharedApplication()
   root_vc = app.keyWindow().rootViewController()
-  root_vc.presentViewController_animated_completion_(vc, True, None)
+  #vc.setModalPresentationStyle_(4)
+  tab_vc = root_vc.detailViewController()
+
+  #root_vc.presentViewController_animated_completion_(vc, True, None)
+  
+  tab_vc.addTabWithViewController_(vc)
   vc.release()
-  print(vc)
 
 
 if __name__ == '__main__':
-  
+
   open_in_safari_vc('http://localhost:8000/')
   try:
     httpd.serve_forever()
   except KeyboardInterrupt:
     httpd.shutdown()
     print('Server stopped')
-
 
