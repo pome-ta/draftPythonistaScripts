63fe06fa369d536fdcc3fb4216931656a515b734
pome-ta <pome-ta@users.noreply.github.com>
2022-01-20 16:12:55 +0900

    server

--- js/soundShader/wk.py
+++ js/soundShader/wk.py
@@ -1,8 +1,17 @@
+import os
+import sys
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
 from objc_util import ObjCClass, on_main_thread, nsurl
 import ui
 
 import pdbg
 
+
+os.chdir(os.path.join(os.path.dirname(__file__), 'public'))
+
+httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
 NSURLRequest = ObjCClass('NSURLRequest')
 
 WKWebView = ObjCClass('WKWebView')
@@ -25,20 +34,42 @@ class WkWeb:
   @on_main_thread
   def viewDidLoad(self):
     #myURL = nsurl('https://www.apple.com')
-    myURL = nsurl('https://qiita.com/syunyo/items/d03a714af4dadd0727a6')
+    #httpd.serve_forever()
+    myURL = nsurl('http://localhost:8000/')
+    
     req = NSURLRequest.requestWithURL_(myURL)
     self.wkwebview.loadRequest_(req)
+    
 
 
 class View(ui.View):
   def __init__(self, *args, **kwargs):
     ui.View.__init__(self, *args, **kwargs)
     self.bg_color = 'maroon'
+    self.serve()
     self.wk = WkWeb()
     self.objc_instance.addSubview_(self.wk.wkwebview)
+    
+  @ui.in_background
+  def serve(self):
+    httpd.serve_forever()
+    
+  def will_close(self):
+    httpd.shutdown()
+    #pass
 
 
 if __name__ == '__main__':
   view = View()
   view.present(style='panel')
+  '''
+  try:
+    httpd.serve_forever()
+  except KeyboardInterrupt:
+    httpd.shutdown()
+    print('Server stopped')
+  '''
+  
+  
+
 
