39594abbb203a90d19d62f0569b77cfa46ae1b7c
pome-ta <pome-ta@users.noreply.github.com>
2022-01-23 17:40:13 +0900

    up

--- js/soundShader/wk.py
+++ js/soundShader/wk.py
@@ -21,13 +21,14 @@ class WkWeb:
     self.wwvconf = WKWebViewConfiguration.new()
     self.loadView()
     self.viewDidLoad()
+    #self.wkwebview.reload()
     #pdbg.state(self.wkwebview)
 
   @on_main_thread
   def loadView(self):
     _frame = ((0.0, 0.0), (100.0, 100.0))
     self.wwvconf.preferences().setValue_forKey_(True, 'allowFileAccessFromFileURLs')
-    self.wkwebview.initWithFrame_configuration_(_frame, self.wwvconf)
+    self.wkwebview.initWithFrame_configuration_(_frame, self.wwvconf).autorelease()
     self.wkwebview.setAutoresizingMask_((1 << 1) | (1 << 4))
 
   @on_main_thread
@@ -37,18 +38,18 @@ class WkWeb:
     url = 'file://' + str(uri.resolve()).replace(' ', '%20')
     #myURL = nsurl(str(url))
     myURL = nsurl(str(uri))
-    req = NSURLRequest.requestWithURL_(myURL)
+    #req = NSURLRequest.requestWithURL_(myURL)
     #iniReq = NSURLRequest.alloc().initWithURL_(myURL)
     
-    #iniReq = NSURLRequest.alloc().initWithURL_cachePolicy_timeoutInterval_(myURL, 0, 100)
+    iniReq = NSURLRequest.alloc().initWithURL_cachePolicy_timeoutInterval_(myURL, 1, 100)
     
     
     
     #initWithURL:cachePolicy:timeoutInterval:
     #pdbg.state(iniReq)
     #req = NSURLRequest.requestWithURL_cachePolicy_timeoutInterval_(myURL, 0, 10)
-    self.wkwebview.loadRequest_(req)
-    #self.wkwebview.loadRequest_(iniReq)
+    #self.wkwebview.loadRequest_(req)
+    self.wkwebview.loadRequest_(iniReq)
     #pdbg.state(self.wkwebview)
     
 
@@ -78,6 +79,8 @@ class View(ui.View):
 if __name__ == '__main__':
   view = View()
   view.present(style='panel')
+  view.wk.wkwebview.reload()
+  
   #pdbg.state(view.wk.wkwebview)
   #view.present()
 
