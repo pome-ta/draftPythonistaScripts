67e4259c59e6b6504a40c592beacfde82c49072c
pome-ta <pome-ta@users.noreply.github.com>
2022-05-29 20:34:13 +0900

    getUrlTitle.py

--- markdown/getUrlTitle.py
+++ markdown/getUrlTitle.py
@@ -1,4 +1,10 @@
-import appex
+from operator import itemgetter
 
-if __name__ == '__main__':
-  pass
+from appex import get_web_page_info, finish
+import clipboard
+
+web_info = get_web_page_info()
+title, url = itemgetter('title', 'url')(web_info)
+clipboard.set(f'[{title}]({url})')
+finish()
+
