110f921708bab70eb57b789b84b34496871ce79c
pome-ta <pome-ta@users.noreply.github.com>
2022-05-30 00:12:14 +0900

    browser

--- markdown/getUrlTitle.py
+++ markdown/getUrlTitle.py
@@ -3,8 +3,12 @@ from operator import itemgetter
 from appex import get_web_page_info, finish
 import clipboard
 
+import webbrowser
+
 web_info = get_web_page_info()
 title, url = itemgetter('title', 'url')(web_info)
 clipboard.set(f'[{title}]({url})')
+
+webbrowser.open('pythonista://')
 finish()
 
