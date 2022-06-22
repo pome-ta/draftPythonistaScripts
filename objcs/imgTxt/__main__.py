bdc5436cd00f63f5b55ff75c23a132dbe2d1781b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-06-23 01:18:03 +0900

    up

--- objcs/imgTxt/__main__.py
+++ objcs/imgTxt/__main__.py
@@ -2,8 +2,13 @@ from objc_util import ObjCClass
 
 import pdbg
 
-
 VNRecognizeTextRequest = ObjCClass('VNRecognizeTextRequest')
 request = VNRecognizeTextRequest.new()
 
-pdbg.state(request)
+# todo: default_`en_US` <- `request.recognitionLanguages()`
+
+VNImageRequestHandler = ObjCClass('VNImageRequestHandler')
+
+
+pdbg.state(request.recognitionLanguages())
+
