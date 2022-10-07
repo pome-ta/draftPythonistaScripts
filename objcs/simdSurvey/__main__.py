beb1325cca7fa0b6ba24f3287500abf7fda96fc6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-10-08 01:09:48 +0900

    up

--- objcs/simdSurvey/__main__.py
+++ objcs/simdSurvey/__main__.py
@@ -1,18 +1,14 @@
-from pathlib import Path
-import webbrowser
+from objc_util import c, load_framework, ObjCClass, ObjCInstance
+import ctypes
 
-from objc_util import ObjCClass
 
-NSBundle = ObjCClass("NSBundle")
 
-p = 'Frameworks'
 
-obj_lib = NSBundle.bundleWithPath_(f'/System/Library/{p}').bundlePath()
-root_dir = Path.expanduser(Path(str(obj_lib)))
+#void vvceil(double *, const double *, const int *);
 
-f = 'Accelerate'
-root_list = sorted(list(root_dir.iterdir()))
-for i in root_list:
-  if f in f'{i.name}':
-    print(i)
+vvceil = c.vvceil
+
+MTLCreateSystemDefaultDevice.argtypes = []
+MTLCreateSystemDefaultDevice.restype = ctypes.c_void_p
+m = ObjCInstance(MTLCreateSystemDefaultDevice())
 
