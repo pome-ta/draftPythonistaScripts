ce33a3712a901f46a95e9c6eb3f7be91c8a17abe
pome-ta <pometarou93@gmail.com>
2022-10-08 17:02:19 +0900

    search

--- objcs/fileDirs/search.py
+++ objcs/fileDirs/search.py
@@ -1,11 +1,26 @@
+import re
 from pathlib import Path
 
-search_str = 'swift'
 
-frameworks_path = '/System/Library/Frameworks'
-frameworks_iter = Path(frameworks_path).iterdir()
-frameworks_list = sorted(list(frameworks_iter))
+def search_frameworks(word: str):
+  p = re.compile(f'{word}', re.IGNORECASE)
+  search_area = ['Frameworks', 'PrivateFrameworks']
+  for area in search_area:
+    _search_print(area, p)
 
-for fw in frameworks_list:
-  if search_str in fw.name:
-    print(fw)
+
+def _search_print(path_name, compiler):
+  path_str = f'/System/Library/{path_name}'
+  search_path = Path(path_str)
+  if not search_path.exists():
+    print('ファイルがありません：終了します')
+    return
+  fw_list = sorted(list(search_path.iterdir()))
+  result = [fw for fw in fw_list if compiler.search(fw.name)]
+  print(f'--- {path_name} :hit {len(result)}')
+  print(*result, sep='\n')
+
+
+if __name__ == '__main__':
+  search_word = 'swift'
+  search_frameworks(search_word)
