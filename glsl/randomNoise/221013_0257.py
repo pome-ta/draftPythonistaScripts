45c976e5e6cd2e3103207f2a8763c49523edd072
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-10-13 14:47:00 +0900

    up

--- glsl/randomNoise/221013_0257.py
+++ glsl/randomNoise/221013_0257.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,8 +10,13 @@ def fractSin11(x: float) -> float:
   return fract(1e3 * math.sin(x))
 
 
+def view_plot(data):
+  plt.plot(np.sin(l * np.pi))
+  plt.show()
 
-l = np.arange(0.0, 10.0, 0.001, dtype=np.float32)
-
-plt.plot(np.sin(l * np.pi))
-plt.show()
+if __name__ == '__main__':
+  q: float = 1e5
+  #m: list = [math.sin((i / (n / 1e2)) * math.pi) for i in range(int(n))]
+  lpy: list = [i / (q / 1e2) for i in range(int(q))]
+  lnp = np.arange(0.0, 10.0, 0.001, dtype=np.float32)
+  
