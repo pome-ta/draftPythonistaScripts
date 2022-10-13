17d2d675741242f60747feec52b1ad67ef621415
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-10-14 00:24:13 +0900

    CMMotionManager

--- iOSbookSampleCode/a04/221013_1840.py
+++ iOSbookSampleCode/a04/221013_1840.py
@@ -6,46 +6,47 @@ import pdbg
 
 NSOperationQueue = ObjCClass('NSOperationQueue')
 CMMotionManager = ObjCClass('CMMotionManager')
-CMMotionManager_ = CMMotionManager.alloc().init()
-
-if not CMMotionManager_.isAccelerometerAvailable():
-  print('Accelerometer in NOT Available.')
-  raise
-
-CMMotionManager_.accelerometerUpdateInterval = 1  # 単位は秒
 
 
-def _handler(_cmd, _data, _error):
-  accelerometerData = ObjCInstance(_data)
-  #pdbg.state(accelerometerData)
-  print('😇----')
-  #pdbg.state(accelerometerData.acceleration())
-  print(accelerometerData.acceleration().a)
+class MotionAccelerometer:
+  def __init__(self, update_interval=0.1):
+    self.accelerometerData = []
+    self.CMMotionManager = CMMotionManager.alloc().init()
+    if not self.CMMotionManager.isAccelerometerAvailable():
+      print('Accelerometer in NOT Available.')
+      raise
+    self.accelerometerUpdateInterval = update_interval
+    
+    self.accelerometer_handler = ObjCBlock(
+      self.__handler,
+      restype=None,
+      argtypes=[ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p])
 
+  def startAccelerometerUpdates(self):
+    self.CMMotionManager.accelerometerUpdateInterval = self.accelerometerUpdateInterval
+    self.CMMotionManager.startAccelerometerUpdatesToQueue_withHandler_(
+      NSOperationQueue.mainQueue(), self.accelerometer_handler)
 
-accelerometer_handler = ObjCBlock(
-  _handler,
-  restype=None,
-  argtypes=[ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p])
+  def stopAccelerometerUpdates(self, timer=3):
+    time.sleep(timer)
+    self.CMMotionManager.stopAccelerometerUpdates()
 
-CMMotionManager_.startAccelerometerUpdatesToQueue_withHandler_(
-  NSOperationQueue.mainQueue(), accelerometer_handler)
+  def append_data(self, data):
+    acceleration = data.acceleration()
+    x = acceleration.a
+    y = acceleration.b
+    z = acceleration.c
+    ts = data.timestamp()
+    self.accelerometerData.append({'x': x, 'y': y, 'z': z, 'at': ts})
+
+  def __handler(self, _cmd, _data, _error):
+    data = ObjCInstance(_data)
+    self.append_data(data)
+
 
 if __name__ == '__main__':
-  #pdbg.state(CMMotionManager_)
-  #pass
-  #accelerometerData = CMMotionManager_.startAccelerometerUpdates()
-  #time.sleep(3)  # 単位は秒
-  #print(accelerometerData)
-  #print(CMMotionManager_.accelerometerData())
-  #pdbg.state(NSOperationQueue)
-  #currentQueue
-  #mainQueue
-  #print(NSOperationQueue.currentQueue())
-  #print(NSOperationQueue.mainQueue())
-  pdbg.state(CMMotionManager_.accelerometerData())
-  time.sleep(3)
-  #pdbg.state(CMMotionManager_.accelerometerData())
-  print(CMMotionManager_.accelerometerData())
-  CMMotionManager_.stopAccelerometerUpdates()
+  ma = MotionAccelerometer()
+  ma.startAccelerometerUpdates()
+  ma.stopAccelerometerUpdates(2)
+  print(ma.accelerometerData)
 
