2edbcebda2864514facf19feb52f73907b70fea4
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-10-18 16:49:06 +0900

    up

--- objcs/AVFoundation/live_camera_view.py
+++ objcs/AVFoundation/live_camera_view.py
@@ -4,23 +4,31 @@
 from objc_util import ObjCClass, CGRect, CGPoint, CGSize
 import ui
 
+AVCaptureSession = ObjCClass('AVCaptureSession')
+AVCaptureDevice = ObjCClass('AVCaptureDevice')
+AVCaptureDeviceInput = ObjCClass('AVCaptureDeviceInput')
+AVCaptureVideoPreviewLayer = ObjCClass('AVCaptureVideoPreviewLayer')
+
 
 class LiveCameraView(ui.View):
   def __init__(self, device=0, *args, **kwargs):
     ui.View.__init__(self, *args, **kwargs)
-    self._session = ObjCClass('AVCaptureSession').alloc().init()
+    self._session = AVCaptureSession.alloc().init()
     self._session.setSessionPreset_('AVCaptureSessionPresetHigh')
-    inputDevices = ObjCClass('AVCaptureDevice').devices()
+    inputDevices = AVCaptureDevice.devices()
     self._inputDevice = inputDevices[device]
 
-    deviceInput = ObjCClass(
-      'AVCaptureDeviceInput').deviceInputWithDevice_error_(
-        self._inputDevice, None)
+    deviceInput = AVCaptureDeviceInput.deviceInputWithDevice_error_(
+      self._inputDevice, None)
+    
     if self._session.canAddInput_(deviceInput):
       self._session.addInput_(deviceInput)
-    self._previewLayer = ObjCClass(
-      'AVCaptureVideoPreviewLayer').alloc().initWithSession_(self._session)
-    self._previewLayer.setVideoGravity_('AVLayerVideoGravityResizeAspectFill')
+    
+    self._previewLayer = AVCaptureVideoPreviewLayer.alloc().initWithSession_(
+      self._session)
+
+    resizeAspectFill = 'AVLayerVideoGravityResizeAspectFill'
+    self._previewLayer.setVideoGravity_(resizeAspectFill)
     #rootLayer = ObjCInstance(self).layer()
     rootLayer = self.objc_instance.layer()
     rootLayer.setMasksToBounds_(True)
