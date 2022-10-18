be851812abee12ccb1c385eefba671c6355828ef
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-10-18 14:31:08 +0900

    up

--- objcs/CoreML/VisionFramework/__main__.py
+++ objcs/CoreML/VisionFramework/__main__.py
@@ -27,7 +27,7 @@ class CameraView(ui.View):
   def __init__(self, frame=CGRect((0, 0), (100, 100)), *args, **kwargs):
     ui.View.__init__(self, *args, **kwargs)
     self.flex = 'WH'
-    #self.bg_color = 'blue'
+    self.bg_color = 'green'
 
     self.previewLayer = AVCaptureVideoPreviewLayer.alloc().init()
     #self.previewLayer.setFrame_(frame)
@@ -37,31 +37,38 @@ class CameraView(ui.View):
     self.objc_instance.layer().addSublayer_(self.previewLayer)
 
     self._overlayLayer = CAShapeLayer.alloc().init()
-    self.showPoints()
+    self.setCAShapeLayer()
 
   def layout(self):
     self.previewLayer.frame = self.objc_instance.bounds()
 
     self._overlayLayer.frame = self.previewLayer.bounds()
+    
+    self.showPoints(self._overlayLayer.frame().size)
 
-  def showPoints(self):
+  def showPoints(self, size):
+    height = size.height
+    width = size.width
+
+    center = CGPoint(width / 2, height / 2)
+    radius = 100.0
+    startAngle = 0.125 * math.pi
+    endAngle = 1.875 * math.pi
+
+    arc = UIBezierPath.alloc().init()
+    arc.addArcWithCenter_radius_startAngle_endAngle_clockwise_(
+      center, radius, startAngle, endAngle, True)
+
+    #pdbg.state(arc.CGPath())
+    self._overlayLayer.setPath_(arc.CGPath())
+
+  def setCAShapeLayer(self):
     self._overlayLayer.setLineWidth_(20.0)
     blueColor = UIColor.blueColor().cgColor()
     cyanColor = UIColor.cyanColor().cgColor()
     self._overlayLayer.setStrokeColor_(blueColor)
     self._overlayLayer.setFillColor_(cyanColor)
     self.previewLayer.addSublayer_(self._overlayLayer)
-    
-    center = CGPoint(200, 200)
-    radius = 100.0
-    startAngle = 0.125 * math.pi
-    endAngle = 1.875 * math.pi
-    
-    arc = UIBezierPath.alloc().init()
-    arc.addArcWithCenter_radius_startAngle_endAngle_clockwise_(center, radius, startAngle, endAngle, True)
-    
-    #pdbg.state(arc.CGPath())
-    self._overlayLayer.setPath_(arc.CGPath())
 
 
 class CameraViewController:
