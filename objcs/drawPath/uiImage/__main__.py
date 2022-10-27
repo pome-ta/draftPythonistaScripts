7930642e3338e42a2adb2488878f978a86c7ed21
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-10-27 12:30:22 +0900

    up

--- objcs/drawPath/uiImage/__main__.py
+++ objcs/drawPath/uiImage/__main__.py
@@ -1,13 +1,17 @@
+import math
 from pathlib import Path
 
-from objc_util import ObjCClass, nsurl, NSData
+from objc_util import ObjCClass, nsurl, NSData, CGPoint
 import ui
 
 import pdbg
 
 UIImageView = ObjCClass('UIImageView')
 UIImage = ObjCClass('UIImage')
+
 CAShapeLayer = ObjCClass('CAShapeLayer')
+UIBezierPath = ObjCClass('UIBezierPath')
+UIColor = ObjCClass('UIColor')
 
 
 def get_image_absolutepath(path):
@@ -34,13 +38,39 @@ class ViewController:
     self.imageView = UIImageView.alloc().initWithImage_(self.originalImage)
     
     self.overlayLayer = CAShapeLayer.alloc().init()
-    pdbg.state(self.overlayLayer.frame().size)
+    self.setupOverlay()
+    self.setCAShapeLayer()
+    #pdbg.state(self.overlayLayer.frame().size)
     
     
     self.previewView.addSubview_(self.imageView)
 
   def setupOverlay(self):
-    pass
+    self.overlayLayer.frame = self.previewView.bounds()
+    self.drawPath()
+    
+  def setCAShapeLayer(self):
+    self.overlayLayer.setLineWidth_(20.0)
+    blueColor = UIColor.blueColor().cgColor()
+    cyanColor = UIColor.cyanColor().cgColor()
+    self.overlayLayer.setStrokeColor_(blueColor)
+    self.overlayLayer.setFillColor_(cyanColor)
+    self.imageView.layer().addSublayer_(self.overlayLayer)
+    
+  def drawPath(self):
+    height = 500
+    width = 500
+
+    center = CGPoint(width / 2, height / 2)
+    radius = 100.0
+    startAngle = 0.125 * math.pi
+    endAngle = 1.875 * math.pi
+
+    arc = UIBezierPath.alloc().init()
+    arc.addArcWithCenter_radius_startAngle_endAngle_clockwise_(
+      center, radius, startAngle, endAngle, True)
+
+    self.overlayLayer.setPath_(arc.CGPath())
 
 
 class View(ui.View):
@@ -57,5 +87,8 @@ class View(ui.View):
 
 if __name__ == '__main__':
   img_file_path = '../../CoreML/VisionFramework/face/img/sample01.png'
+  #img_file_path = '../../CoreML/VisionFramework/face/img/multi-face.png'
+  
+  
   view = View()
 
