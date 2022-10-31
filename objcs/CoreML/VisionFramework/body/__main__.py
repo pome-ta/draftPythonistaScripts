c34f44bde6ce7b23361f01da2a7aa74d3e8d6d1c
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-10-31 13:26:36 +0900

    up

--- objcs/CoreML/VisionFramework/body/__main__.py
+++ objcs/CoreML/VisionFramework/body/__main__.py
@@ -1,10 +1,11 @@
 import ctypes
 
-from objc_util import c, ObjCClass, ObjCInstance, create_objc_class
+from objc_util import c, ObjCClass, ObjCInstance, create_objc_class, CGRect
 import ui
 
 import pdbg
 
+AVCaptureVideoPreviewLayer = ObjCClass('AVCaptureVideoPreviewLayer')
 AVCaptureSession = ObjCClass('AVCaptureSession')
 AVCaptureDevice = ObjCClass('AVCaptureDevice')
 AVCaptureDeviceInput = ObjCClass('AVCaptureDeviceInput')
@@ -14,35 +15,70 @@ dispatch_get_current_queue = c.dispatch_get_current_queue
 dispatch_get_current_queue.restype = ctypes.c_void_p
 
 
-class CameraViewController:
-  def __init__(self, _previewView):
-    self.previewView = _previewView
-    self.cameraSession = None
-    # xxx: ObjCInstance 必要？
-    self.cameraQueue = ObjCInstance(dispatch_get_current_queue())
-    self.viewDidAppear()
 
+def parseCGRect(cg_rect: CGRect) -> tuple:
+  _origin = cg_rect.origin
+  _size = cg_rect.size
+  origin_x = _origin.x
+  origin_y = _origin.y
+  size_width = _size.width
+  size_height = _size.height
+  return (origin_x, origin_y, size_width, size_height)
+
+
+
+
+class CameraViewController:
+  def __init__(self, cameraView):
+    self.cameraView = cameraView
+    self.previewLayer = None  # AVCaptureVideoPreviewLayer
+    self.cameraSession = None  # AVCaptureSession
+    self.delegate = self.create_sampleBufferDelegate()
+    self.cameraQueue = ObjCInstance(dispatch_get_current_queue())
+    #self.cameraQueue = dispatch_get_current_queue()
+    self.viewDidAppear()
+  
+  def layout(self):
+    self.previewLayer.frame = self.cameraView.bounds()
+  
   def viewDidAppear(self):
     self.prepareAVSession()
+    self.previewLayer = AVCaptureVideoPreviewLayer.alloc().initWithSession_(self.cameraSession)
+    _resizeAspectFill = 'AVLayerVideoGravityResizeAspectFill'
+    self.previewLayer.videoGravity = _resizeAspectFill
+    self.cameraView.layer().addSublayer_(self.previewLayer)
+    
+    self.previewLayer.frame = self.cameraView.bounds()
+    #pdbg.state(self.previewView)
+    
+    self.cameraSession.startRunning()
+    
+    
+    
+    
+  def viewWillDisappear(self):
+    self.cameraSession.stopRunning()
 
   def prepareAVSession(self):
     session = AVCaptureSession.alloc().init()
     session.beginConfiguration()
     _Preset_high = 'AVCaptureSessionPresetHigh'
     session.setSessionPreset_(_Preset_high)
-    
+
     videoDevice = AVCaptureDevice.defaultDeviceWithMediaType_('vide')
     deviceInput = AVCaptureDeviceInput.deviceInputWithDevice_error_(
       videoDevice, None)
-    
+
     if session.canAddInput_(deviceInput):
       session.addInput_(deviceInput)
-    
+
     dataOutput = AVCaptureVideoDataOutput.alloc().init()
-    
-    
-    
-    pdbg.state(session)
+    if session.canAddOutput_(dataOutput):
+      session.addOutput_(dataOutput)
+      dataOutput.setSampleBufferDelegate_queue_(self.delegate,
+                                                self.cameraQueue)
+    session.commitConfiguration()
+    self.cameraSession = session
 
   def create_sampleBufferDelegate(self):
     # --- /delegate
@@ -65,13 +101,16 @@ class View(ui.View):
     self.bg_color = 'maroon'
 
     # xxx: 先に呼ぶ？
-    self.present(style='fullscreen', orientations=['portrait'])
+    #self.present(style='fullscreen', orientations=['portrait'])
     self.cvc = CameraViewController(self.objc_instance)
+  def layout(self):
+    self.cvc.layout()
 
   def will_close(self):
-    pass
+    self.cvc.viewWillDisappear()
 
 
 if __name__ == '__main__':
   view = View()
+  view.present(style='fullscreen', orientations=['portrait'])
 
--- objcs/CoreML/VisionFramework/body/memo.md
+++ objcs/CoreML/VisionFramework/body/memo.md
@@ -3,6 +3,32 @@
 
 [iOS 14 Vision Body Pose Detection: Count Squat Reps in a SwiftUI Workout App | by Philipp Gehrke | Better Programming](https://betterprogramming.pub/ios-14-vision-body-pose-detection-count-squat-reps-in-a-workout-c88991f7cad4)
 
+
+# 📝 2022/10/31
+
+
+## `frame` と`bounds`
+
+親のサイズ継承をする場合、`frame` と`bounds` どっちをどっちに投げればいいのだろうか？
+
+
+### `layout` メソッドを独自に生やす
+
+
+`ui.View` の継承として、考えてみている。
+
+`layout` を親の`ui.View` が処理された時に呼び出されるようにして、画面サイズを取り敢えず調整している
+
+``` .py
+  def layout(self):
+    self.previewLayer.frame = self.cameraView.bounds()
+    print(f'cam: {parseCGRect(self.cameraView.frame())}')
+    print(f'pre: {parseCGRect(self.previewLayer.frame())}')
+```
+
+今のところ、問題は出ていないし、すっきりするので「取り敢えずヨシ！」してる
+
+
 # 📝 2022/10/30
 
 顔も手も、キャプチャで実装できてないけどやってみる😂
@@ -11,3 +37,9 @@
 ## 書き方
 
 本来`private` のものは、`_hoge` として書き進めた方が良さそう。しかし、`self._hoge` を呼び出すのにPythonista だと予測が出ないので、`self.hoge` と書く
+
+
+
+[UIViewControllerのライフサイクル - Qiita](https://qiita.com/motokiee/items/0ca628b4cc74c8c5599d)
+
+[UIViewControllerまとめ - Qiita](https://qiita.com/edo_m18/items/189acd18f1ecc368b5b0)
