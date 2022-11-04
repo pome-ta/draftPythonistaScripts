a43e0ac825906b74c5d03ab123e1c400c7fb14ea
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-11-05 01:51:41 +0900

    up

--- articles/AdventCalendar2022/articlesCreate.py
+++ articles/AdventCalendar2022/articlesCreate.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from shutil import rmtree
 
 dash = '---\n'
 tags = 'tags: Pythonista3 Python3\n'
@@ -10,14 +11,15 @@ def create_article(day, path):
   day_count = f'{day+1:02}'
   body = [
     dash,
-    f'title: {day_count}日目の記事(メモ欄)\n',
+    f'title: {day_count}目の記事(メモ欄)\n',
     tags,
     author,
     slide,
     dash,
-    f'この記事は、[Pythonista3 Advent Calendar 2022](https://qiita.com/advent-calendar/2022/pythonista3) の{day_count}日目の記事です。\n',
+    f'この記事は、[Pythonista3 Advent Calendar 2022](https://qiita.com/advent-calendar/2022/pythonista3) の{day_count}日目の記事です。\n\n## \n',
   ]
-  article = Path(path, f'day{day_count}.md')
+  article = Path(path, f'day{day_count}', f'day{day_count}.md')
+  article.parent.mkdir(parents=True, exist_ok=True)
   with article.open(mode='w', encoding='utf-8') as f:
     f.writelines(body)
 
@@ -25,7 +27,7 @@ def create_article(day, path):
 def setup_folder_path(dir_name):
   target_path = Path('./', dir_name)
   if (target_path.exists() or target_path.is_dir()):
-    [file.unlink() for file in target_path.iterdir() if file.is_file()]
+    rmtree(target_path)
   target_path.mkdir(exist_ok=True)
   return target_path
 
@@ -40,5 +42,4 @@ if __name__ == '__main__':
   folder_name = 'content'
   event_period = 25
   main(folder_name, event_period)
-
 
