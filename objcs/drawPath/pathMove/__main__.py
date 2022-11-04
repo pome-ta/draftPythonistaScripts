20577548a992e1ad277423cc6929ae927dc33061
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-11-04 20:44:49 +0900

    up

--- objcs/drawPath/pathMove/__main__.py
+++ objcs/drawPath/pathMove/__main__.py
@@ -1,22 +1,31 @@
-from pathlib import Path
-
-from objc_util import ObjCClass, nsurl, NSData, CGRect, CGPoint, CGSize
+from objc_util import ObjCClass, ObjCInstance, CGRect, CGPoint, CGSize
 import ui
 
 import pdbg
 
+CAShapeLayer = ObjCClass('CAShapeLayer')
+UIBezierPath = ObjCClass('UIBezierPath')
+UIColor = ObjCClass('UIColor')
 
-class ViewController:
-  def __init__(self):
-    pass
+
+class ShapeLayerView(ui.View):
+  def __init__(self, frame=CGRect((0, 0), (100, 100)), *args, **kwargs):
+    ui.View.__init__(self, *args, **kwargs)
+    self.bg_color = 'green'
+    self.flex = 'WH'
+    self.overlayLayer = CAShapeLayer.new()
+    self.objc_instance.layer().addSublayer_(self.overlayLayer)
+
+  def layout(self):
+    self.overlayLayer.frame = self.objc_instance.bounds()
 
 
 class View(ui.View):
   def __init__(self, *args, **kwargs):
     ui.View.__init__(self, *args, **kwargs)
     self.bg_color = 'maroon'
-
-    #self.view_controller = ViewController(self.objc_instance)
+    self.shapelayer_view = ShapeLayerView()
+    self.add_subview(self.shapelayer_view)
 
 
 if __name__ == '__main__':
--- objcs/drawPath/pathMove/memo.md
+++ objcs/drawPath/pathMove/memo.md
@@ -2,4 +2,9 @@
 
 ## 複数の物を動かしたい
 
+### `update` について
 
+`ui.View` の`update` ではなく、`AVCaptureVideoDataOutputSampleBufferDelegate` を使ってobjc 側で、呼び出す方式にする
+
+
+つっかかりを確認するため
