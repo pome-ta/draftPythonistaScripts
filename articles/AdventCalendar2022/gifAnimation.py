c1f6fffe5c71c22655c63d8529d899ff8ad763f0
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-11-07 00:17:54 +0900

    gifAnimation.py

--- articles/AdventCalendar2022/gifAnimation.py
+++ articles/AdventCalendar2022/gifAnimation.py
@@ -1,59 +1,75 @@
-#from images2gif import writeGif
 from pathlib import Path
-import images2gif
-from PIL import Image as ImageP
+import re
 import shutil
+
 import arrow
 
-from objc_util import ObjCClass
-import photos
 from objc_util import ObjCClass, ObjCInstance
-import pdbg
-
-PHAsset = ObjCClass('PHAsset')
-
-#img_assets = photos.get_assets('video')
-img_assets = photos.get_assets()
-pick_img = photos.pick_asset(img_assets)
-
-img_objc = ObjCInstance(pick_img._objc_ptr)
-#pdbg.state(objc_img)
-print(img_objc.mainFileURL())
-file_url = img_objc.mainFileURL()
-file_path = str(file_url).strip('file:')
-print(file_path)
-
-gif_path = Path(file_path)
-
-#print(gif_path.exists())
-_utc = arrow.utcnow().to('JST')
-_now = _utc.format('YYMMDD_HHmmss')
-root_path = Path(f'./gif{_now}.gif')
-
-shutil.copy(str(gif_path), str(root_path))
-
-'''
-gif_data = images2gif.readGif(str(gif_path))
-
-_utc = arrow.utcnow().to('JST')
-_now = _utc.format('YYMMDD_HHmmss')
-images2gif.writeGif(f'gif{_now}.gif', gif_data, duration=0.2, nq=0)
-'''
-
-#gif_read = images2gif.readGif(f'{img_objc.mainFileURL()}')
+import photos
+import editor
+import clipboard
 
 
-#gif_img = pick_img.get_image()
-#gif_img.show()
+def get_feedback_generator():
+  """
+  call feedback ex:
+  `UIImpactFeedbackGenerator.impactOccurred()`
+  """
+  style = 4  # 0-4 
+  UIImpactFeedbackGenerator = ObjCClass('UIImpactFeedbackGenerator').new()
+  UIImpactFeedbackGenerator.prepare()
+  UIImpactFeedbackGenerator.initWithStyle_(style)
+  return UIImpactFeedbackGenerator
 
-#gif_bytes = pick_img.get_image_data()
-#gifimg = ImageP.open(gif_bytes)
-#hoge = images2gif.readGif(gifimg)
 
-#gifimg.save('gif.gif')
-#writeGif('gif.gif', gifimg)
+def get_assetURLstr(asset):
+  phAsset = ObjCInstance(asset._objc_ptr)
+  mainFileURL = phAsset.mainFileURL()
+  assetURL_str = str(mainFileURL).strip('file:')
+  return assetURL_str
 
-#bytes = imgs.getvalue()
-#print(len(bytes))
 
+def get_folder_path(path='imgs'):
+  data_path = Path(editor.get_path()).parent
+  target_path = Path(f'{data_path}', path)
+  if not target_path.exists():
+    target_path.mkdir()
+  return [target_path, path]
+
+
+def create_filename(name=None, extension='gif'):
+  if name:
+    return f'{name}.{extension}'
+  else:
+    _utc = arrow.utcnow().to('JST')
+    _now = _utc.format('YYMMDD_HHmmss')
+    return f'img{_now}.{extension}'
+
+
+def set_seveto_clipboard(gif_url):
+  origin_path = Path(gif_url)
+  root_path, parent_name = get_folder_path()
+  save_name = create_filename()
+  save_path = Path(root_path, save_name).absolute()
+
+  clipboard.set(f'![screenshot](./{parent_name}/{save_name})')
+
+  shutil.copy(str(origin_path), str(save_path))
+
+
+def main():
+  image_assets = photos.get_assets()
+  pick_asset = photos.pick_asset(image_assets)
+  if not pick_asset:
+    return
+
+  pickURL_str = get_assetURLstr(pick_asset)
+  if re.search(r'\.gif$', pickURL_str, flags=re.IGNORECASE):
+    feedback = get_feedback_generator()
+    set_seveto_clipboard(pickURL_str)
+    feedback.impactOccurred()
+
+
+if __name__ == '__main__':
+  main()
 
