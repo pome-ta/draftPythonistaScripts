9a47c3febf628d5ac861989f043d8b497054b953
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-11-06 19:20:14 +0900

    up

--- articles/AdventCalendar2022/gifAnimation.py
+++ articles/AdventCalendar2022/gifAnimation.py
@@ -2,6 +2,8 @@
 from pathlib import Path
 import images2gif
 from PIL import Image as ImageP
+import shutil
+import arrow
 
 from objc_util import ObjCClass
 import photos
@@ -16,14 +18,27 @@ pick_img = photos.pick_asset(img_assets)
 
 img_objc = ObjCInstance(pick_img._objc_ptr)
 #pdbg.state(objc_img)
-#print(img_objc.mainFileURL())
+print(img_objc.mainFileURL())
 file_url = img_objc.mainFileURL()
 file_path = str(file_url).strip('file:')
 print(file_path)
 
 gif_path = Path(file_path)
 
-print(gif_path.exists())
+#print(gif_path.exists())
+_utc = arrow.utcnow().to('JST')
+_now = _utc.format('YYMMDD_HHmmss')
+root_path = Path(f'./gif{_now}.gif')
+
+shutil.copy(str(gif_path), str(root_path))
+
+'''
+gif_data = images2gif.readGif(str(gif_path))
+
+_utc = arrow.utcnow().to('JST')
+_now = _utc.format('YYMMDD_HHmmss')
+images2gif.writeGif(f'gif{_now}.gif', gif_data, duration=0.2, nq=0)
+'''
 
 #gif_read = images2gif.readGif(f'{img_objc.mainFileURL()}')
 
