e794cfcfe7e93deb58d5da19b65476864aa38f41
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-11-11 15:06:37 +0900

    up

--- sceneUi/__main__.py
+++ sceneUi/__main__.py
@@ -1,46 +1,8 @@
 import scene
 import ui
 
-TITLE = 'プログラムでお絵描き'
 
-BG_COLOR = 0.872
-TINT_COLOR = 0.128
-TITLE_COLOR = 0.872
-AF_COLOR = TINT_COLOR
-
-
-class Canvas(scene.Scene):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.background_color = BG_COLOR
-
-  @ui.in_background
-  def setup(self):
-    self.set_line(1)
-
-  def set_line(self, dire):
-    w2, h2 = self.size / 2
-    path = ui.Path()
-    path.move_to(w2 - 128, h2 - 128)
-    path.line_to(w2 + 128, h2 + 128)
-    line = scene.ShapeNode(parent=self)
-    line.path = path
-    line.stroke_color = 'red'
-    line.position = self.size / 2
-
-  @ui.in_background
-  def update(self):
-    pass
-
-  def rgba(self, r, g, b, a=255):
-    raba = []
-    for j in r, g, b, a:
-      t = j / 255
-      rgba += t,
-    return tuple(rgba)
-
-
-class View(ui.View):
+class MainView(ui.View):
   def __init__(self):
     self.bg_color = TINT_COLOR
     self.tint_color = TITLE_COLOR
@@ -70,6 +32,9 @@ class View(ui.View):
     ui.animate(animation, duration=.3)
 
   def draw(self):
+    self.set_background()
+
+  def set_background(self):
     w = self.width
     h = self.height * .96
     x = self.width / 2 - w / 2
@@ -82,14 +47,14 @@ class View(ui.View):
     self.scene_view.height = self.height * .96
     self.scene_view.x = self.width / 2 - self.scene_view.width / 2
 
-  def create_navbtn(self, icon):
+  def create_button(self, icon):
     btn_icon = ui.Image.named(icon)
     btn = ui.ButtonItem(image=btn_icon)
     return btn
 
   def setup_nav(self):
     save_icon = 'iob:ios7_download_outline_32'
-    save_btn = self.create_navbtn(save_icon)
+    save_btn = self.create_button(save_icon)
     save_btn.action = self.save_view
     return [save_btn]
 
@@ -97,14 +62,51 @@ class View(ui.View):
   def save_view(self, sender):
     raw_img = self.canvas.view._debug_quicklook_()
     png = ui.Image.from_data(raw_img, 2.0)
-    # fixme: 保存時に1.0 になっちゃう
     png.show()
 
+class Canvas(scene.Scene):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.background_color = BG_COLOR
 
-view = View()
-view.present(
-  style='fullscreen',
-  orientations='portrait',
-  title_bar_color=TINT_COLOR,
-  title_color=TITLE_COLOR)
+  @ui.in_background
+  def setup(self):
+    self.set_line(50)
+
+  def set_line(self, dire):
+    w2, h2 = self.size / 2
+    path = ui.Path()
+    path.move_to(w2 - 128, h2 - 128)
+    path.line_to(w2 + 128, h2 + 128)
+    line = scene.ShapeNode(parent=self)
+    line.path = path
+    line.stroke_color = 'red'
+    line.position = self.size / 2
+
+  @ui.in_background
+  def update(self):
+    pass
+
+  def rgba(self, r, g, b, a=255):
+    raba = []
+    for j in r, g, b, a:
+      t = j / 255
+      rgba += t,
+    return tuple(rgba)
+
+
+if __name__ == '__main__':
+  TITLE = 'プログラムでお絵描き'
+
+  BG_COLOR = 0.872
+  TINT_COLOR = 0.128
+  TITLE_COLOR = 0.872
+  AF_COLOR = TINT_COLOR
+
+  view = MainView()
+  view.present(
+    style='fullscreen',
+    orientations='portrait',
+    title_bar_color=TINT_COLOR,
+    title_color=TITLE_COLOR)
 
