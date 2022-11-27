4190aeff4b3ccbd9293dd9b6a488b0ca1bf91602
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-11-27 22:36:09 +0900

    up

--- objcs/fileDirs/webBrowserView.py
+++ objcs/fileDirs/webBrowserView.py
@@ -5,11 +5,16 @@ import webbrowser
 
 uri_path = ObjCClass('NSBundle').mainBundle().resourcePath()
 
-#webbrowser.open('pythonista3:/' + '/..' * os.getcwd().count('/') + str(uri_path))
+uri_str = '/..' * os.getcwd().count('/') + str(uri_path)
+
+
+webbrowser.open('pythonista3:/' + uri_str)
 #webbrowser.open('pythonista3://../..')
+'''
 PA2UITheme = ObjCClass('PA2UITheme')
 
 theme_dict = PA2UITheme.sharedTheme().userThemesPath()
 theme_dict_path = str(theme_dict) + '/..'
 
-webbrowser.open('pythonista3:' + theme_dict_path)
+webbrowser.open('pythonista3:/' + theme_dict_path)
+'''
