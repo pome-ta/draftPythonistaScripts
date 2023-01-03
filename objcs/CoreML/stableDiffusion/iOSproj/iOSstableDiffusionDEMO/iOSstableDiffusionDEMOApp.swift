30fc8d68a824faae154f28a0aa2294aeec4b272e
pome-ta <pometarou93@gmail.com>
2023-01-03 22:12:36 +0900

    up

---
+++ objcs/CoreML/stableDiffusion/iOSproj/iOSstableDiffusionDEMO/iOSstableDiffusionDEMOApp.swift
@@ -0,0 +1,18 @@
+//
+//  iOSstableDiffusionDEMOApp.swift
+//  iOSstableDiffusionDEMO
+//  
+//  Created by pome-ta on 2023/01/03
+//  
+//
+
+import SwiftUI
+
+@main
+struct iOSstableDiffusionDEMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
