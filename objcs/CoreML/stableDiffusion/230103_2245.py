d2904fb983ca4356ccaa93619dd8c8622ad645b4
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-01-09 00:53:11 +0900

    up

--- objcs/CoreML/stableDiffusion/230103_2245.py
+++ objcs/CoreML/stableDiffusion/230103_2245.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from objc_util import ObjCClass, NSBundle
+from objc_util import ObjCClass, NSBundle, nsurl
 
 import pdbg
 
@@ -7,9 +7,32 @@ MLModelConfiguration = ObjCClass('MLModelConfiguration')
 
 models_root_path = './models/coreml-stable-diffusion-v1-4_original_compiled'
 
+
 class ResourceURLs:
-  def __init__(self):
+  def __init__(self, root_paths):
+    self.textEncoderURL: nsurl
+    self.unetURL: nsurl
+    self.unetChunk1URL: nsurl
+    self.unetChunk2URL: nsurl
+    self.decoderURL: nsurl
+    self.safetyCheckerURL: nsurl
+    self.vocabURL: nsurl
+    self.mergesURL: nsurl
+
+  def _init(self, paths):
     pass
+
+
+i = [
+  'TextEncoder.mlmodelc',
+  'Unet.mlmodelc',
+  'UnetChunk1.mlmodelc',
+  'UnetChunk2.mlmodelc',
+  'VAEDecoder.mlmodelc',
+  'SafetyChecker.mlmodelc',
+  'vocab.json',
+  'merges.txt',
+]
 
 root_path = Path(models_root_path)
 
