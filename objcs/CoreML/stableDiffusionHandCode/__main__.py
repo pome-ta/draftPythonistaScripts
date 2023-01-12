e502019a933497a717840e6bdc75bfc609d53ea1
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-01-12 11:22:06 +0900

    up

--- objcs/CoreML/stableDiffusionHandCode/__main__.py
+++ objcs/CoreML/stableDiffusionHandCode/__main__.py
@@ -1,13 +1,11 @@
 from pathlib import Path
+#import stableDiffusion
+from stableDiffusion.pipeline import StableDiffusionPipeline
 
 
-if __name__ == '__main__':
+
+#if __name__ == '__main__':
   # todo: Pythonista3 かPython で切り分け
-
-  models_root_path = './models/coreml-stable-diffusion-v1-4_original_compiled'
-  root_path = Path(models_root_path)
-  path = Path('./')
-  print('hoge')
-  print(path)
-  print(path.resolve())
-  print(path)
+models_root_path = './models/coreml-stable-diffusion-v1-4_original_compiled'
+root_path = Path(models_root_path)
+hoge = StableDiffusionPipeline()
