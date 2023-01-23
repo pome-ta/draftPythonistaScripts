167646f6b2f693e3ede5391f075c8d8020fa415a
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-01-23 20:39:06 +0900

    `MLDictionaryFeatureProvider`

--- objcs/CoreML/stableDiffusionHandCode/pyStableDiffusion/pipeline/TextEncoder.py
+++ objcs/CoreML/stableDiffusionHandCode/pyStableDiffusion/pipeline/TextEncoder.py
@@ -11,6 +11,7 @@ import pdbg
 
 
 MLMultiArray = ObjCClass('MLMultiArray')
+MLDictionaryFeatureProvider = ObjCClass('MLDictionaryFeatureProvider')
 
 class TextEncoder:
   def __init__(self, tokenizer: BPETokenizer, url: Path, configuration):
@@ -53,8 +54,13 @@ class TextEncoder:
     #pdbg.state(self.hoge)
     self.inputArray = MLMultiArray.alloc().initWithShape_dataType_error_(inputShape, 16, None)
     [self.inputArray.setObject_atIndexedSubscript_(obj, index) for index, obj in enumerate(floatIds)]
+
+    #pdbg.state(self.inputArray)
+    self.inputFeatures = MLDictionaryFeatureProvider.new().initWithDictionary_error_(({inputName: self.inputArray}), None)
+    pdbg.state(self.inputFeatures.dictionary())
     
-    pdbg.state(self.inputArray)
+    
+    
 
 
 
