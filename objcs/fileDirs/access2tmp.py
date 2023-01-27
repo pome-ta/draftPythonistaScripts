987cb95b8ad869396546447085587349bca6cc1f
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-01-27 11:09:30 +0900

    up

--- objcs/fileDirs/access2tmp.py
+++ objcs/fileDirs/access2tmp.py
@@ -1,6 +1,5 @@
 import webbrowser
 from objc_util import ObjCClass
-import pdbg
 
 NSFileManager = ObjCClass('NSFileManager')
 
@@ -9,7 +8,7 @@ def get_temporaryDirectory_path() -> str:
   nsFileManager = NSFileManager.new()
   # todo: `file://` で取得してしまうため、`uri` 呼び出し
   temp_dir = nsFileManager.temporaryDirectory().uri()
-  return str(temp_dir)  # todo: 文字列キャスト
+  return str(temp_dir)
 
 
 def get_toplevel_path(up_level: int=9) -> str:
@@ -19,5 +18,7 @@ def get_toplevel_path(up_level: int=9) -> str:
 
 
 if __name__ == '__main__':
+  # todo: 1番上まで戻り、tmp ディレクトリへ
   path = get_toplevel_path() + get_temporaryDirectory_path()
   webbrowser.open(f'pythonista3://{path}')
+
