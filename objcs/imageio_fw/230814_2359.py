77f4798c45fd9533615bff34c381a0490f2f70e8
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-08-15 00:05:15 +0900

    up

--- objcs/imageio_fw/230814_2359.py
+++ objcs/imageio_fw/230814_2359.py
@@ -1,1 +1,28 @@
+import re
+from pathlib import Path
 
+
+def search_frameworks(word: str):
+  p = re.compile(f'{word}', re.IGNORECASE)
+  search_area = ['Frameworks', 'PrivateFrameworks']
+  print(f'search: `{word}`')
+  for area in search_area:
+    _search_print(area, p)
+
+
+def _search_print(path_name, compiler):
+  path_str = f'/System/Library/{path_name}'
+  search_path = Path(path_str)
+  if not search_path.exists():
+    print('ファイルがありません:終了します')
+    return
+  fw_list = sorted(list(search_path.iterdir()))
+  result = [fw for fw in fw_list if compiler.search(fw.name)]
+  
+  print(f'--- {path_name} :hit {len(result)}')
+  print(*result, sep='\n')
+
+
+if __name__ == '__main__':
+  search_word = 'image'
+  search_frameworks(search_word)
---
+++ objcs/imageio_fw/memo.md
@@ -0,0 +1,12 @@
+```
+search: `image`
+--- Frameworks :hit 3
+/System/Library/Frameworks/CoreImage.framework
+/System/Library/Frameworks/ImageCaptureCore.framework
+/System/Library/Frameworks/ImageIO.framework
+--- PrivateFrameworks :hit 3
+/System/Library/PrivateFrameworks/DiskImages.framework
+/System/Library/PrivateFrameworks/DiskImages2.framework
+/System/Library/PrivateFrameworks/IOImageLoader.framework
+
+```
