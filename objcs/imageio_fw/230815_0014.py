59ebac969c7cf43ec38616ef049029e3f84ff399
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-08-15 11:57:56 +0900

    up

--- objcs/imageio_fw/230815_0014.py
+++ objcs/imageio_fw/230815_0014.py
@@ -1,4 +1,4 @@
-from objc_util import ObjCClass,c
+from objc_util import ObjCClass, c
 import pdbg
 
 #CGImageSourceCopyProperties = c.CGImageSourceCopyProperties
@@ -7,22 +7,37 @@ PHAsset = ObjCClass('PHAsset')
 PHAssetCollection = ObjCClass('PHAssetCollection')
 PHFetchOptions = ObjCClass('PHFetchOptions')
 
+UIImage = ObjCClass('UIImage')
+NSData = ObjCClass('NSData')
+
 # PHAssetCollectionTypeSmartAlbum = 2
 # PHAssetCollectionSubtypeSmartAlbumUserLibrary = 209
 
 # PHAssetCollectionSubtypeSmartAlbumRecentlyAdded = 206
 
-result = PHAssetCollection.fetchAssetCollectionsWithType_subtype_options_(2, 209, None)
+result = PHAssetCollection.fetchAssetCollectionsWithType_subtype_options_(
+  2, 209, None)
 
 assetCollection = result.firstObject()
 
-fetchResult = PHAsset.fetchAssetsInAssetCollection_options_(assetCollection, None)
+fetchResult = PHAsset.fetchAssetsInAssetCollection_options_(
+  assetCollection, None)
 
 #first_object = fetchResult.firstObject()
 #first_object = fetchResult.indexOfObject_(0)
 
 last_object = fetchResult.lastObject()
+assetsLibraryURL_url = last_object.assetsLibraryURL()
+mainFileURL_url = last_object.mainFileURL()
 
 #pdbg.state(assetCollection)
 #pdbg.state(fetchResult)
-pdbg.state(last_object.assetsLibraryURL())
+#pdbg.state(last_object.assetsLibraryURL())
+url_data = NSData.dataWithContentsOfURL_(mainFileURL_url)
+#absoluteString',
+# 'absoluteURL',
+#print(photo_url)
+#print(photo_url.absoluteString())
+#pdbg.state(photo_url.absoluteString())
+#pdbg.state(last_object.mainFileURL())
+
