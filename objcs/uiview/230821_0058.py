d53dfb673a4be08210b1381441a413eeb4625347
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-08-21 14:06:20 +0900

    up

--- objcs/uiview/230821_0058.py
+++ objcs/uiview/230821_0058.py
@@ -1,7 +1,15 @@
+# tab ? のやつ出す
+
+
 from objc_util import ObjCClass
 
 import ui
 
 
+UITabBar = ObjCClass('UITabBar')
+
 class View(ui.View):
-  def
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(self, *args, **kwargs)
+
