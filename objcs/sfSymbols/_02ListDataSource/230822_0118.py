58680facc923c1a69ea55bc182b3b30f94bcc350
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-08-22 16:39:52 +0900

    up

--- objcs/sfSymbols/_02ListDataSource/230822_0118.py
+++ objcs/sfSymbols/_02ListDataSource/230822_0118.py
@@ -1,13 +1,38 @@
 import ui
 
 
-class View(ui.View):
+class SearchField(ui.View):
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
+    self.bg_color = 1
+    self.bar = ui.TextField()
+    self.bar.clear_button_mode = 'always'
+    self.bar.placeholder = 'search symbol name'
+    self.add_subview(self.bar)
+
+  def layout(self):
+    _, _, w, h = self.frame
+    self.bar.width = w * 0.88
+    self.bar.height = h * 0.64
+    self.bar.center = self.center
+
+
+class MainView(ui.View):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.bg_color = 'maroon'
+    self.search_field = SearchField()
+    self.add_subview(self.search_field)
+
+  def layout(self):
+    _, _, w, h = self.frame
+    self.search_field.width = w
+    self.search_field.height = 48
 
 
 if __name__ == '__main__':
-  view = View()
+  view = MainView()
   view.present(style='fullscreen', orientations=['portrait'])
 
