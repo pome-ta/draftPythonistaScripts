8d26d296a0ffb44f52f11fcfa2c0394e98c30166
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-01 03:59:57 +0900

    up

--- objcs/sfSymbols/_04layout/230831_1505.py
+++ objcs/sfSymbols/_04layout/230831_1505.py
@@ -6,17 +6,21 @@ import pdbg
 UIView = ObjCClass('UIView')
 UIViewController = ObjCClass('UIViewController')
 
-uiview = UIView.new()
-pdbg.state(uiview)
+
+uiview = UIView.new().initWithFrame_(((0.0, 0.0), (200.0, 120.0))).autorelease()
+uiview.setAutoresizingMask_((1 << 1) | (1 << 4))
+#pdbg.state(uiview)
+
 
 class PyView(ui.View):
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
-    
+    self.objc_instance.addSubview_(uiview)
 
 
 if __name__ == '__main__':
   py_view = PyView()
-  #py_view.present(style='fullscreen', orientations=['portrait'])
+  py_view.present(style='fullscreen', orientations=['portrait'])
+  pdbg.state(uiview)
 
