5a52b6480aca7a7d73c9ee814bb9933a1d34bc35
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-02 00:13:21 +0900

    up

--- objcs/sfSymbols/_04layout/230901_0405.py
+++ objcs/sfSymbols/_04layout/230901_0405.py
@@ -3,13 +3,9 @@ import ui
 
 import pdbg
 
+UIColor = ObjCClass('UIColor')
 UIView = ObjCClass('UIView')
 UIViewController = ObjCClass('UIViewController')
-
-uiview = UIView.new().initWithFrame_(
-  ((0.0, 0.0), (200.0, 120.0))).autorelease()
-uiview.setAutoresizingMask_((1 << 1) | (1 << 4))
-#pdbg.state(uiview)
 
 
 class ObjcView(ui.View):
@@ -17,7 +13,17 @@ class ObjcView(ui.View):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.flex = 'WH'
-    self.objc_instance.addSubview_(uiview)
+
+    _gf = ((0.0, 0.0), (256.0, 256.0))
+
+    self.ground_view = UIView.new()
+    self.ground_view.initWithFrame_(_gf)  #.autorelease()
+
+    #self.ground_view = UIView.new().initWithFrame_(_gf).autorelease()
+    self.ground_view.backgroundColor = UIColor.redColor()
+    self.ground_view.setAutoresizingMask_((1 << 1) | (1 << 4))
+
+    self.objc_instance.addSubview_(self.ground_view)
 
 
 class PyView(ui.View):
@@ -25,13 +31,21 @@ class PyView(ui.View):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.objc_view: ui.View = ObjcView()
-    
+    #self.objc_view.flex = 'WH'
+
     self.add_subview(self.objc_view)
+    #self.present()
+
+  def layout(self):
+    #pdbg.state(self.objc_view.ground_view)
+    _, _, w, h = self.frame
+
+    #self.objc_view.width = w
+    #self.objc_view.height = h
 
 
 if __name__ == '__main__':
-
-  py_view = PyView()
-  py_view.present(style='fullscreen', orientations=['portrait'])
-  pdbg.state(uiview)
+  view = PyView()
+  view.present(style='fullscreen', orientations=['portrait'])
+  #view.present()
 
