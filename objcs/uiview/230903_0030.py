d7854222ddc14f09303c9a1c44b84c2f89ada9ae
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-03 00:30:43 +0900

    up

---
+++ objcs/uiview/230903_0030.py
@@ -0,0 +1,14 @@
+from objc_util import ObjCClass, ObjCInstance
+import ui
+
+import pdbg
+
+UIColor = ObjCClass('UIColor')
+UIView = ObjCClass('UIView')
+UIViewController = ObjCClass('UIViewController')
+
+#pdbg.state(UIViewController.new())
+view = ui.View()
+#pdbg.state(ObjCInstance(view))
+pdbg.state(UIView.new())
+
