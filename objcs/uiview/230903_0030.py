29cebb9f9378ef89e3d6a35d5f322e25c5394578
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-03 02:32:12 +0900

    up

--- objcs/uiview/230903_0030.py
+++ objcs/uiview/230903_0030.py
@@ -5,10 +5,23 @@ import pdbg
 
 UIColor = ObjCClass('UIColor')
 UIView = ObjCClass('UIView')
-UIViewController = ObjCClass('UIViewController')
 
-#pdbg.state(UIViewController.new())
-view = ui.View()
-#pdbg.state(ObjCInstance(view))
-pdbg.state(UIView.new())
+
+class PyView(ui.View):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.bg_color = 'cyan'
+    self.ground = ui.View()
+    self.ground.bg_color = 'yellow'
+    self.ground.flex = 'WH'
+    #self.add_subview(self.ground)
+    #pdbg.state(self.ground.objc_instance)
+    self.objc_instance.addSubview_(self.ground.objc_instance)
+
+
+if __name__ == '__main__':
+  view = PyView()
+  view.present(style='fullscreen', orientations=['portrait'])
+  #view.present()
 
