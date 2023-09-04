82698117eb73fe8b9da74f4a00f0bb88ec22b365
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-05 03:28:01 +0900

    up

---
+++ objcs/uiview/230904_1640.py
@@ -0,0 +1,106 @@
+from objc_util import ObjCClass, create_objc_class
+import ui
+
+import pdbg
+
+UIView = ObjCClass('UIView')
+UISearchBar = ObjCClass('UISearchBar')
+UIColor = ObjCClass('UIColor')
+
+# xxx: 検索時のcell 選択も視野に`delegate` を作っていく
+# xxx: まずは、通常の入力検知
+
+
+class SearchTableViewController(object):
+
+  def __init__(self):
+    pass
+
+  def searchBar_delegate(self):
+    # --- `UISearchBarDelegate` Methods
+    def searchBar_textDidChange_(_self, _cmd, _searchBar, _searchText) -> None:
+      pass
+
+    def searchBar_shouldChangeTextInRange_replacementText_(
+        _self, _cmd, _searchBar, _range, _text) -> bool:
+      return True
+
+    def searchBarShouldBeginEditing_(_self, _cmd, _searchBar) -> bool:
+      return True
+
+    def searchBarTextDidBeginEditing_(_self, _cmd, _searchBar) -> None:
+      pass
+
+    def searchBarShouldEndEditing_(_self, _cmd, _searchBar) -> bool:
+      return True
+
+    def searchBarTextDidEndEditing_(_self, _cmd, _searchBar) -> None:
+      pass
+
+
+class ObjcUI(object):
+
+  def __init__(self):
+    self.view = UIView.new()
+    self.viewDidLoad()
+    #self.view.addSubview_(self.view_w)
+    self.view.addSubview_(self.sb)
+
+  def viewDidLoad(self):
+    frame = ((0.0, 0.0), (100.0, 100.0))
+    self.view.setFrame_(frame)
+    self.view.setAutoresizingMask_((1 << 1) | (1 << 4))
+    self.view.backgroundColor = UIColor.redColor()
+    self.view.autorelease()
+
+    self.sb = UISearchBar.new()
+    self.sb.setFrame_(frame)
+    self.sb.backgroundColor = UIColor.cyanColor()
+    # [UISearchBarStyle | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uisearchbarstyle?language=objc)
+    '''
+    0 : UISearchBarStyleDefault
+    1 : UISearchBarStyleProminent
+    2 : UISearchBarStyleMinimal
+    '''
+    # xxx: `UISearchBarStyleMinimal` 以外落ちる
+    self.sb.searchBarStyle = 2
+    self.sb.placeholder = 'ほげ☺️'
+    self.sb.size = (100.0, 56.0)
+
+    self.sb.setAutoresizingMask_(1 << 1)
+    self.sb.autorelease()
+
+    self.view_w = UIView.new()
+    self.view_w.setFrame_(frame)
+    self.view_w.setAutoresizingMask_(1 << 1)
+    self.view_w.backgroundColor = UIColor.cyanColor()
+    self.view_w.autorelease()
+
+  def layout(self):
+    pass
+
+
+class PyView(ui.View):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.bg_color = 'maroon'
+
+    self.objc_view = ObjcUI()
+    self.objc_instance.addSubview_(self.objc_view.view)
+
+  def layout(self):
+    size = self.objc_view.view.frame().size
+
+    width = size.width
+    height = size.height
+    #pdbg.state(size)
+    #print(width, height)
+
+
+if __name__ == '__main__':
+  view = PyView()
+  #view.present(style='fullscreen', orientations=['portrait'])
+  view.present(style='fullscreen')
+  #view.present()
+
