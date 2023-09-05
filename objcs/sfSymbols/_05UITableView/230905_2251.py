98636f21e469074f36e169cf29071fefbe4905ea
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-06 02:21:51 +0900

    up

---
+++ objcs/sfSymbols/_05UITableView/230905_2251.py
@@ -0,0 +1,80 @@
+from objc_util import ObjCClass, ObjCInstance, create_objc_class
+import ui
+
+import pdbg
+
+UIView = ObjCClass('UIView')
+UITableView = ObjCClass('UITableView')
+UIColor = ObjCClass('UIColor')
+
+
+class ObjcControlView(object):
+
+  def __init__(self):
+    self.view = UIView.new()
+
+    self.table_view: 'UITableView'
+    self.viewDidLoad()
+    self.view.addSubview_(self.table_view)
+
+  def init_UITableView(self):
+    frame = ((0.0, 0.0), (100.0, 100.0))
+    self.table_view = UITableView.new()
+    # xxx: 2度`frame` 指定している。`init` からは`frame` が反映されないため
+    self.table_view.initWithFrame_style_(frame, 1)
+    self.table_view.setFrame_(frame)
+    
+    # [UITableViewStyle | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uitableviewstyle?language=objc)
+    '''
+    0 : UITableViewStylePlain
+    1 : UITableViewStyleGrouped
+    2 : UITableViewStyleInsetGrouped
+    '''
+    #self.table_view.style = 0
+    
+    
+    
+    
+    
+    self.table_view.setAutoresizingMask_((1 << 1) | (1 << 4))
+    self.table_view.autorelease()
+    pdbg.state(self.table_view)
+
+  def viewDidLoad(self):
+    frame = ((0.0, 0.0), (100.0, 100.0))
+    self.view.setFrame_(frame)
+    self.view.setAutoresizingMask_((1 << 1) | (1 << 4))
+    self.view.backgroundColor = UIColor.redColor()
+    self.view.autorelease()
+
+    self.init_UITableView()
+
+  def layout(self):
+    pass
+
+
+class PyView(ui.View):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.bg_color = 'maroon'
+
+    self.objc_view = ObjcControlView()
+    self.objc_instance.addSubview_(self.objc_view.view)
+
+  def layout(self):
+    #size = self.objc_view.view.frame().size
+    size = self.objc_view.table_view.frame().size
+
+    width = size.width
+    height = size.height
+    #pdbg.state(size)
+    print(width, height)
+
+
+if __name__ == '__main__':
+  view = PyView()
+  #view.present(style='fullscreen', orientations=['portrait'])
+  view.present(style='fullscreen')
+  #view.present()
+
