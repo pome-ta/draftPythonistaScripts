6e5ff0fbe96de5fcf6016b81adb88000a5e1f065
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-06 18:29:11 +0900

    up

--- objcs/sfSymbols/_05UITableView/230906_1319.py
+++ objcs/sfSymbols/_05UITableView/230906_1319.py
@@ -5,15 +5,76 @@ import pdbg
 
 UIView = ObjCClass('UIView')
 UITableView = ObjCClass('UITableView')
+UITableViewCell = ObjCClass('UITableViewCell')
 UIColor = ObjCClass('UIColor')
 
+all_items = [
+  'Swift',
+  'Java',
+  'Ruby',
+  'C++',
+  'C',
+  'C#',
+  'Python',
+  'Perl',
+  'JavaScript',
+  'PHP',
+  'Scratch',
+  'Scala',
+  'COBOL',
+  'Curl',
+  'Dart',
+  'HTML',
+  'CSS',
+  'Ruby on Rails',
+  'TypeScript',
+  'ECMAScript',
+  'Jython',
+  'CPython',
+  'PyPy',
+  'IronPython',
+]
 
 
 class TableViewController(object):
 
-  def __init__(self):
-    self._data_source: 'UITableViewDataSource'
-    
+  def __init__(self, items: list = []):
+    self.items = items
+    self._tableData_source: 'UITableViewDataSource'
+    self.init_tableData_source()
+
+  @property
+  def tableData_source(self):
+    return self._tableData_source
+
+  def init_tableData_source(self):
+    # --- `UITableViewDataSource` Methods
+    def tableView_numberOfRowsInSection_(_self, _cmd, _tableView, _section):
+      raise Exception
+      return len(self.items)
+
+    def tableView_cellForRowAtIndexPath_(_self, _cmd, _tableView, _indexPath):
+      #tableView = ObjCInstance(tableView)
+      #cell: 'UITableViewCell'
+      #print('h')
+      #cell = tableView.dequeueReusableCell_withIdentifier_for_('cell', _indexPath) if
+
+      #cell = tableView.dequeueReusableCell_withIdentifier_for_()
+      #dequeueReusableCellWithIdentifier_forIndexPath_
+      return UITableViewCell.new().initWithStyle_reuseIdentifier_(0, 'cell')
+
+    # --- `UITableViewDataSource` set up
+    _methods = [
+      tableView_numberOfRowsInSection_,
+      tableView_cellForRowAtIndexPath_,
+    ]
+    _protocols = ['UITableViewDataSource']
+
+    tableData_source = create_objc_class(name='tableData_source',
+                                         methods=_methods,
+                                         protocols=_protocols)
+    self._tableData_source = tableData_source.new()
+
 
 class ObjcControlView(object):
 
@@ -21,6 +82,9 @@ class ObjcControlView(object):
     self.view = UIView.new()
 
     self.table_view: 'UITableView'
+    self.controllers = TableViewController(all_items)
+
+    
     self.viewDidLoad()
     self.view.addSubview_(self.table_view)
 
@@ -38,10 +102,14 @@ class ObjcControlView(object):
 
     # xxx: 2度`frame` 指定している。`init` からは`frame` が反映されないため
     self.table_view.setFrame_(frame)
+    #self.table_view.dataSource = self.controllers.tableData_source
+    self.table_view.setDataSource_(self.controllers.tableData_source)
+    
 
     self.table_view.setAutoresizingMask_((1 << 1) | (1 << 4))
     self.table_view.autorelease()
     #pdbg.state(self.table_view)
+    #pdbg.state(self.controllers.tableData_source)
 
   def viewDidLoad(self):
     frame = ((0.0, 0.0), (100.0, 100.0))
