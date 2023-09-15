a90f8ea1e712cacdf25eb1e451cc1b4c6385a4ea
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-15 11:43:20 +0900

    up

--- objcs/navigationItem/230915_0255.py
+++ objcs/navigationItem/230915_0255.py
@@ -9,8 +9,12 @@ class PyView(ui.View):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.bg_color = 'maroon'
+    self.name = 'ほげ☺️'
+    self.b = ui.ButtonItem(image=ui.Image('iob:alert_24'))
+    self.right_button_items = [self.b]
+    
     #self.nv = ui.NavigationView()
-    #pdbg.state(self.nv.objc_instance)    
+    #pdbg.state(self.nv.objc_instance)
 
   def layout(self):
     pass
