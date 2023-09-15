9ee9aa3fd9486ca6aa87efb80e021c91454b1545
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-16 01:13:54 +0900

    up

--- objcs/UITabBar/230915_1514.py
+++ objcs/UITabBar/230915_1514.py
@@ -3,14 +3,31 @@ import ui
 
 import pdbg
 
+UIView = ObjCClass('UIView')
 UITabBar = ObjCClass('UITabBar')
 
+UIColor = ObjCClass('UIColor')
 
-class ObjcControl(object):
+
+class ObjcController(object):
 
   def __init__(self):
     self.tmp_frame = ((0.0, 0.0), (100.0, 100.0))
-    self.view = UIView.new()
+    self.view = UIView.new().autorelease()
+    self.tab_bar = UITabBar.new().autorelease()
+    
+    self.viewDidLoad()
+
+  def viewDidLoad(self):
+    self.view.setFrame_(self.tmp_frame)
+    self.view.setAutoresizingMask_((1 << 1) | (1 << 4))
+    self.view.backgroundColor = UIColor.redColor()
+    
+    self.view.addSubview_(self.tab_bar)
+    
+
+  def _init_UITabBar(self):
+    pass
 
 
 class PyView(ui.View):
@@ -18,6 +35,9 @@ class PyView(ui.View):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.bg_color = 'maroon'
+
+    self.objc_controller = ObjcController()
+    self.objc_instance.addSubview_(self.objc_controller.view)
 
   def layout(self):
     pass
