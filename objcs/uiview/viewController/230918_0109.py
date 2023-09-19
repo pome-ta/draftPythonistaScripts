5b7c560e259da35ca310ba1c95db621faa3b74dc
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-19 18:15:54 +0900

    up

--- objcs/uiview/viewController/230918_0109.py
+++ objcs/uiview/viewController/230918_0109.py
@@ -2,34 +2,15 @@ import ui
 
 
 class View(ui.View):
-  def __init__(self):
-    self.bg_color = 1
 
-  def did_load(self):
-    pass
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.bg_color = 'maroon'
 
   def will_close(self):
     pass
 
-  def draw(self):
-    pass
-
   def layout(self):
-    pass
-
-  def touch_began(self, touch):
-    pass
-
-  def touch_moved(self, touch):
-    pass
-
-  def touch_ended(self, touch):
-    pass
-
-  def keyboard_frame_will_change(self, frame):
-    pass
-
-  def keyboard_frame_did_change(self, frame):
     pass
 
 
