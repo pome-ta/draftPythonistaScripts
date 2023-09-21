3acaf8d97364c1b5a03359e324bcfd80ac1c99a8
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-21 17:19:35 +0900

    up

--- objcs/uiview/viewController/230921_1703.py
+++ objcs/uiview/viewController/230921_1703.py
@@ -1,10 +1,18 @@
 from objc_util import ObjCClass
 
 import pdbg
-
+'''
 UIApplication = ObjCClass('UIApplication')
 app = UIApplication.sharedApplication()
 keyWindow = app.keyWindow()
 #windows = app.windows()
 rootViewController = keyWindow.rootViewController()
 pdbg.state(rootViewController)
+'''
+
+
+class MyUIViewController(object):
+
+  def __init__(self):
+    pass
+
