bf077b8c7440481647909216522437dda953d805
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-25 02:34:06 +0900

    up

--- objcs/uiview/viewController/230924_2007.py
+++ objcs/uiview/viewController/230924_2007.py
@@ -4,6 +4,8 @@
 
 # https://forum.omz-software.com/topic/2271/beta-suggestion-safariviewcontroller/9
 
+
+from pprint import pprint
 from objc_util import ObjCClass, nsurl, on_main_thread
 import pdbg
 
@@ -12,17 +14,7 @@ SFSafariViewController = ObjCClass('SFSafariViewController')
 
 
 @on_main_thread
-def open_in_safari_vc(url):
-  sfSafariViewController = SFSafariViewController.alloc()
-  # xxx: `new` だと取れない
-  #sfSafariViewController = SFSafariViewController.new()
-  '''
-  vc = SFSafariViewController.alloc().initWithURL_entersReaderIfAvailable_(
-    nsurl(url), True)
-  '''
-  #vc = sfSafariViewController.initWithURL_entersReaderIfAvailable_(nsurl(url), True)
-  vc = sfSafariViewController.initWithURL_(nsurl(url))
-
+def open_in_safari_vc(vc):
   app = UIApplication.sharedApplication()
 
   if app.keyWindow():
@@ -32,17 +24,19 @@ def open_in_safari_vc(url):
     print('f')
     window = app.windows().firstObject()
 
+  print('# --- autolayoutTrace')
+  pprint(window._autolayoutTrace())
+  print('\n'*4)
   root_vc = window.rootViewController()
 
   while root_vc.presentedViewController():
     print('w')
     root_vc = root_vc.presentedViewController()
-  
+
   #pdbg.state(vc.navigationItem())
-  
+
   root_vc.presentViewController_animated_completion_(vc, True, None)
-  vc.release()
-  pdbg.state(vc.navigationItem().rightBarButtonItems())
+  #vc.release()
 
 
 def create_safari_vc(url):
@@ -55,8 +49,29 @@ def create_safari_vc(url):
   '''
   #vc = sfSafariViewController.initWithURL_entersReaderIfAvailable_(nsurl(url), True)
   vc = sfSafariViewController.initWithURL_(nsurl(url))
+  return vc
+
+@on_main_thread
+def check_vc():
+  app = UIApplication.sharedApplication()
+
+  if app.keyWindow():
+    #print('t')
+    window = app.keyWindow()
+  else:
+    print('f')
+    window = app.windows().firstObject()
+
+  print('# --- autolayoutTrace')
+  pprint(window._autolayoutTrace())
+  
+  
+
 
 if __name__ == '__main__':
   u = 'https://github.com/pome-ta/Pythonista3AdventCalendar2022sampleCode/blob/main/day00_requests/qiitaAPIv2.py'
-  open_in_safari_vc(u)
+  _vc = create_safari_vc(u)
+  open_in_safari_vc(_vc)
+  check_vc()
+  #pdbg.state(_vc.navigationItem().title())
 
