eb6d6fdc83d9e100328cdc0b3f9f97307f5c8739
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-09-26 18:54:40 +0900

    up

---
+++ objcs/navigationItem/230926_0102.py
@@ -0,0 +1,86 @@
+from objc_util import ObjCClass, ObjCInstance, create_objc_class, on_main_thread
+
+import pdbg
+
+UIViewController = ObjCClass('UIViewController')
+
+
+class ObjcUIViewController:
+
+  def __init__(self):
+    self._this: UIViewController
+    self._override()
+
+  def _override(self):
+    # --- `UIViewController` Methods
+    def viewDidLoad(_self, _cmd):
+      #print('viewDidLoad')
+      this = ObjCInstance(_self)
+      view = this.view()
+      #pdbg.state(view.window())
+
+    def viewWillAppear_(_self, _cmd, _animated):
+      #print('viewWillAppear')
+      pass
+
+    def viewDidAppear_(_self, _cmd, _animated):
+      #print('viewDidAppear')
+      this = ObjCInstance(_self)
+      view = this.view()
+      window = view.window()
+      pdbg.state(window)
+
+    def viewWillDisappear_(_self, _cmd, _animated):
+      #print('viewWillDisappear')
+      pass
+
+    def viewDidDisappear_(_self, _cmd, _animated):
+      #print('viewDidDisappear')
+      pass
+
+    def viewWillLayoutSubviews(_self, _cmd):
+      #print('viewWillLayoutSubviews')
+      pass
+
+    def viewDidLayoutSubviews(_self, _cmd):
+      #print('viewDidLayoutSubviews')
+      pass
+
+    # --- `UIViewController` set up
+    _methods = [
+      viewDidLoad,
+      viewWillAppear_,
+      viewDidAppear_,
+      viewWillDisappear_,
+      viewDidDisappear_,
+      viewWillLayoutSubviews,
+      viewDidLayoutSubviews,
+    ]
+
+    create_kwargs = {
+      'name': '_vc',
+      'superclass': UIViewController,
+      'methods': _methods,
+    }
+    _vc = create_objc_class(**create_kwargs)
+    self._this = _vc.new()
+
+  @classmethod
+  def new(cls) -> ObjCInstance:
+    _cls = cls()
+    return _cls._this
+
+
+@on_main_thread
+def present_objc(vc):
+  app = ObjCClass('UIApplication').sharedApplication()
+  window = app.keyWindow()
+  root_vc = window.rootViewController()
+  
+  root_vc.presentViewController_animated_completion_(vc, True, None)
+
+
+if __name__ == '__main__':
+  ovc = ObjcUIViewController.new()
+  present_objc(ovc)
+
