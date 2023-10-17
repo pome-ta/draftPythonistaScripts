6f0e36bae6b3ac3f6e6944f162b69cc88e25c30c
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-10-17 13:31:31 +0900

    up

--- objcs/simdSurvey/231017_0211.py
+++ objcs/simdSurvey/231017_0211.py
@@ -1,17 +1,21 @@
 import ctypes
 
-from objc_util import c, load_framework, ObjCClass, ObjCInstance
+from objc_util import c, load_framework, ObjCClass, ObjCInstance,on_main_thread
 
 
 # まだ動かない
 _vvsqrtf = c.vvsqrtf
 _vvsqrtf.argtypes = [ctypes.c_float, ctypes.c_float, ctypes.c_int32]
-_vvsqrtf.restype = ctypes.c_void_p
+#_vvsqrtf.restype = None
 
+@on_main_thread
+def main():
+  x = 1000.0
+  y = 1.0
+  n = 1
+  _vvsqrtf(x,y,n)
 
-x = 
-
-
+main()
 
 '''
 simd_equal = c.simd_equal
