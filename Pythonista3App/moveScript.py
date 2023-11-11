9f72150b0cda46bd4ccdac2443fbbadc69ba788d
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-11-11 15:07:37 +0900

    moveScript.py

--- Pythonista3App/moveScript.py
+++ Pythonista3App/moveScript.py
@@ -1,17 +1,12 @@
 from pathlib import Path
-import site
 import shutil
+import sys
 
-print(site.getsitepackages())
 
-print(site.getusersitepackages())
+def get_pythonista_modules_path(sys_paths: list[str]) -> str:
+  _mp, *_ = [_sp for _sp in sys_paths if _sp.endswith('pythonista')]
+  return _mp
 
-getsitepackages = site.getusersitepackages()
 
-pack_path = Path(getsitepackages)
+pythonista_modules_path = get_pythonista_modules_path(sys.path)
 
-pack_list= [pack for pack in pack_path.iterdir()]
-
-#print(list(pack_path.glob()))
-
-#/private/var/containers/Bundle/Application/99EB2042-EF33-4FDA-9808-9886DC80C7CC/Pythonista3.app/Frameworks/Py3Kit.framework/pylib/site-packages/pythonista/cb.py
