5162a8cae5803de01b83cec0cfff25fd03bdf930
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-11-17 16:35:21 +0900

    block

--- objcs/AVAudioEngine/231117_1511.py
+++ objcs/AVAudioEngine/231117_1511.py
@@ -1,5 +1,21 @@
+# [A simple synthesizer class in Swift · GitHub](https://gist.github.com/larsaugustin/1ba0b01ace8772cf5ecbda8f4e3cf63d)
+
 from objc_util import ObjCClass, ObjCInstance
 import pdbg
 
+AVAudioEngine = ObjCClass('AVAudioEngine')
+AVAudioSourceNode = ObjCClass('AVAudioSourceNode')
+
+pdbg.state(AVAudioEngine)
+
+
+class Synthesizer:
+
+  def __init__(self):
+    self.audioEngine = 0
+    self.time = 0.0
+
+
 if __name__ == '__main__':
   pass
+
--- objcs/AVAudioEngine/memo.md
+++ objcs/AVAudioEngine/memo.md
@@ -4,3 +4,22 @@
 
 [A simple synthesizer class in Swift · GitHub](https://gist.github.com/larsaugustin/1ba0b01ace8772cf5ecbda8f4e3cf63d)
 
+
+## 実装
+
+`block` 処理の解決やら、落ちる原因を調べたい
+
+一から書き換えて、呼び出し方法の是非も考えていく
+
+最終的には、`ui` 排除も考えていきたい
+
+
+### `AVAudioEngine`
+
+これは、`.new()` 呼び出しでいいかな？
+
+## `didSet` ?
+
+数値をインタラクティブにいじる時かな？
+
+[【Swift】willSet, didSetの使い方](https://zenn.dev/tomoshimizu/articles/ca732ea57201b8)
