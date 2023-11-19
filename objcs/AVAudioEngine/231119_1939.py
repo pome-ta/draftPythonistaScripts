5a5de2fa2716aaaaca855b51413e57b6816bdaa9
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-11-20 00:26:25 +0900

    AVAudioFormat

---
+++ objcs/AVAudioEngine/231119_1939.py
@@ -0,0 +1,36 @@
+# [A simple synthesizer class in Swift · GitHub](https://gist.github.com/larsaugustin/1ba0b01ace8772cf5ecbda8f4e3cf63d)
+
+from objc_util import ObjCClass, ObjCInstance
+import pdbg
+
+AVAudioEngine = ObjCClass('AVAudioEngine')
+AVAudioSourceNode = ObjCClass('AVAudioSourceNode')
+AVAudioFormat = ObjCClass('AVAudioFormat')
+
+
+class Synthesizer:
+
+  def __init__(self):
+    self.audioEngine: AVAudioEngine
+    self.sourceNode: AVAudioSourceNode
+    self.time = 0.0
+    self.frequencyRamp = 0.0
+    self.currentFrequency = 0.0
+
+  def _setup(self):
+    self.audioEngine = AVAudioEngine.new()
+    format = self.audioEngine.outputNode().inputFormatForBus(0)
+
+    inputFormat = AVAudioFormat.alloc().initWithCommonFormat(
+      format.commonFormat(),
+      sampleRate=format.sampleRate(),
+      channels=1,
+      interleaved=format.isInterleaved())
+    #pdbg.state(inputFormat)
+    pdbg.state(format.sampleRate())
+
+
+if __name__ == '__main__':
+  synthesizer = Synthesizer()
+  synthesizer._setup()
+
