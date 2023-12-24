fb0c86fbeedc0511b92f9acf7edff987ec434f8b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-12-24 16:16:15 +0900

    read test

--- objcs/sfSymbols/_10categories/231224_0028.py
+++ objcs/sfSymbols/_10categories/231224_0028.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import webbrowser
 
+import editor
+
 
 def get_toplevel_path(up_level: int=9) -> str:
   origin_path = './'
@@ -10,5 +12,15 @@ def get_toplevel_path(up_level: int=9) -> str:
 CoreGlyphs_path = '/System/Library/CoreServices/CoreGlyphs.bundle/'
 
 coreGlyphs_dir = Path(get_toplevel_path(),CoreGlyphs_path)
+#webbrowser.open(f'pythonista3:/{coreGlyphs_dir}')
 
-webbrowser.open(f'pythonista3://{coreGlyphs_dir}')
+#coreGlyphs_dir = Path(CoreGlyphs_path, 'name_aliases.strings')
+
+#editor.open_file(coreGlyphs_dir.resolve())
+
+#print(coreGlyphs_dir.resolve())
+coreGlyphs_dir  = Path(CoreGlyphs_path)
+for i in coreGlyphs_dir.iterdir():
+  #print(i)
+  #print(i.read_text(encode='utf8'))
+  print(i.read_bytes())
