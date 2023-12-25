993f1ea1ae67a41e4d9a5f79167d79085c63a967
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-12-25 13:53:50 +0900

    data dump

--- objcs/sfSymbols/_10categories/231224_2232.py
+++ objcs/sfSymbols/_10categories/231224_2232.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import plistlib
+import json
 
 CoreGlyphs_ROOT = '/System/Library/CoreServices/CoreGlyphs.bundle/'
 HOME_PATH = Path().cwd()
 ROOT_PATH = Path(CoreGlyphs_ROOT)
+
 
 def get_data(path: Path | str) -> list | dict:
   _data_path = Path(path)
@@ -11,21 +13,31 @@ def get_data(path: Path | str) -> list | dict:
   return _loads
 
 
-def copy_write_data(path: Path, base_path: str = 'CoreGlyphs') -> None:
+def copy_write_data(path: Path, base_path: Path) -> None:
   if not (path.is_file() and path.suffix != '.car'):
     return
   print(path.name)
-
-
-
+  
+  file_data = None
+  
+  try:
+    file_data = json.dumps(get_data(path), ensure_ascii=False, indent=2)
+    
+  except :
+    pass
+  
+  if not file_data:
+    return 
+  file_name = path.name
+  new_file_path = Path(base_path, file_name)
+  new_file_path.write_text(file_data, encoding='utf-8')
 
 
 if __name__ == '__main__':
   target_parent = 'CoreGlyphs'
   parent_path = Path(target_parent)
-  if parent_path.exists():
-    print('ok')
-  bundles = list(ROOT_PATH.iterdir())
+  None if parent_path.exists() else parent_path.mkdir()
   
-  for bundle in bundles:
-    copy_write_data(bundle)
+  for bundle in ROOT_PATH.iterdir():
+    copy_write_data(bundle, parent_path)
+
