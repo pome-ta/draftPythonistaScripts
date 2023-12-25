76cc024af8a47d751abbb7638b4105ab231d51cf
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-12-26 00:44:00 +0900

    up

---
+++ objcs/sfSymbols/_10categories/231226_0023.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import plistlib
+
+CoreGlyphs_ROOT = '/System/Library/CoreServices/CoreGlyphs.bundle/'
+
+pickup_plists = [
+  'categories.plist',
+  'symbol_categories.plist',
+  'symbol_search.plist',
+]
+
