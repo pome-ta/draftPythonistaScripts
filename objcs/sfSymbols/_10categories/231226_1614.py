5152a9f9a39f4ee8b1fa800c3509999001808fd2
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-12-26 20:06:48 +0900

    bundles

--- objcs/sfSymbols/_10categories/231226_1614.py
+++ objcs/sfSymbols/_10categories/231226_1614.py
@@ -21,10 +21,32 @@ class DictDotNotation(dict):
 
 
 #paths = [bundle.name for bundle in Path(CoreGlyphs_ROOT).iterdir() if bundle.is_file() and bundle.suffix != '.car']
-ignore_suffix = '.car'
+ignore_suffix = [
+  '.car',
+]
+
+ignore_name = [
+  'Info.plist',
+]
+
+
+def get_data(path: Path | str) -> list | dict:
+  _data_path = Path(path) if type(path) == 'str' else path
+  _loads = plistlib.loads(_data_path.read_bytes())
+  return _loads
+
+
+def is_ignore(path: Path) -> bool:
+  return not path.is_file() or (path.suffix
+                                in ignore_suffix) or (path.name in ignore_name)
+
+
 paths = {
-  bundle.stem: bundle.name
-  for bundle in Path(CoreGlyphs_ROOT).iterdir()
-  if bundle.is_file() and bundle.suffix != ignore_suffix
+  bundle.stem: get_data(bundle)
+  for bundle in Path(CoreGlyphs_ROOT).iterdir() if not is_ignore(bundle)
 }
+
+bundles = DictDotNotation(paths)
+
+print(bundles.categories)
 
