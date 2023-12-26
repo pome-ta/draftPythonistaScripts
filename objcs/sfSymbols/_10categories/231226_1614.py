9d68f48731132e77eadae3ff8e1cf5462c43be84
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-12-26 16:23:10 +0900

    up

---
+++ objcs/sfSymbols/_10categories/231226_1614.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import plistlib
+
+CoreGlyphs_ROOT = '/System/Library/CoreServices/CoreGlyphs.bundle/'
+
+CoreGlyphs_names: dict = {}
+CoreGlyphs_path: dict = {}
+
+pickup_plists = [
+  'categories.plist',
+  'symbol_categories.plist',
+  'symbol_search.plist',
+]
+
+
+class DictDotNotation(dict):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.__dict__ = self
+
+
+#paths = [bundle.name for bundle in Path(CoreGlyphs_ROOT).iterdir() if bundle.is_file() and bundle.suffix != '.car']
+ignore_suffix = '.car'
+paths = {
+  bundle.stem: bundle.name
+  for bundle in Path(CoreGlyphs_ROOT).iterdir()
+  if bundle.is_file() and bundle.suffix != ignore_suffix
+}
+
