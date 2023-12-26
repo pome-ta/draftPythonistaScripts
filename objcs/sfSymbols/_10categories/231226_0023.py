d670cc7e2d18926ce3e5d4cad474d69c7d0db178
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-12-26 12:43:36 +0900

    up

--- objcs/sfSymbols/_10categories/231226_0023.py
+++ objcs/sfSymbols/_10categories/231226_0023.py
@@ -3,9 +3,19 @@ import plistlib
 
 CoreGlyphs_ROOT = '/System/Library/CoreServices/CoreGlyphs.bundle/'
 
+CoreGlyphs_names: dict = {}
+CoreGlyphs_path: dict = {}
+
 pickup_plists = [
   'categories.plist',
   'symbol_categories.plist',
   'symbol_search.plist',
 ]
+
+
+class DictDotNotation(dict):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.__dict__ = self
 
