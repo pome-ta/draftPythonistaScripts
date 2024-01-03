bbbd41b1a320515704e621f25e454be7053a76ac
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-01-03 14:38:16 +0900

    UIStackView

--- objcs/uiview/peraIchi/240103_1428.py
+++ objcs/uiview/peraIchi/240103_1428.py
@@ -248,27 +248,24 @@ IS_LAYOUT_DEBUG = True
 
 dummy_img_uri = '/private/var/containers/Bundle/Application/99EB2042-EF33-4FDA-9808-9886DC80C7CC/Pythonista3.app/Media/Images/test/Boat@2x.png'
 
-
 UIStackView = ObjCClass('UIStackView')
-pdbg.state(UIStackView.new())
+#pdbg.state(UIStackView.new())
 
 
-
-
-class WrapView:
+class ObjcView:
 
   def __init__(self, *args, **kwargs):
-    self.view = UIView.alloc()
+    self.instance = UIView.alloc()
     CGRectZero = CGRect((0.0, 0.0), (0.0, 0.0))
-    self.view.initWithFrame_(CGRectZero)
+    self.instance.initWithFrame_(CGRectZero)
 
   def _init(self):
 
     if IS_LAYOUT_DEBUG:
-      self.view.layer().setBorderWidth_(1.0)
-    self.view.setTranslatesAutoresizingMaskIntoConstraints_(False)
+      self.instance.layer().setBorderWidth_(1.0)
+    self.instance.setTranslatesAutoresizingMaskIntoConstraints_(False)
 
-    return self.view
+    return self.instance
 
   @classmethod
   def new(cls, *args, **kwargs) -> ObjCInstance:
@@ -276,57 +273,20 @@ class WrapView:
     return _cls._init()
 
 
-class WrapImageView(WrapView):
+class ObjcStackView(ObjcView):
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
-    self.view = UIImageView.alloc().initWithImage_(kwargs['image'])
-
-
-class WrapLabelView(WrapView):
-
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.view = UILabel.new()
-    self.view.setText_(kwargs['text'])
-    self.view.sizeToFit()
-
-
-class WrapTextFieldView(WrapView):
-
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.view = UITextField.new()
-
-
-class WrapScrollView(WrapView):
-
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.view = UIScrollView.alloc()
+    self.instance = UIStackView.alloc()
     CGRectZero = CGRect((0.0, 0.0), (0.0, 0.0))
-    self.view.initWithFrame_(CGRectZero)
+    self.instance.initWithFrame_(CGRectZero)
 
 
-class WrapSwitch(WrapView):
-
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.view = UISwitch.new()
-
-
-class WrapButton(WrapView):
+class ObjcImageView(ObjcView):
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
-    self.view = UIButton.new()
-    title = kwargs['title']
-    config = UIButtonConfiguration.tintedButtonConfiguration()
-    config.setTitle_(title)
-    config.setBaseBackgroundColor_(UIColor.systemPinkColor())
-    config.setBaseForegroundColor_(UIColor.systemGreenColor())
-
-    self.view.setConfiguration_(config)
+    self.instance = UIImageView.alloc().initWithImage_(kwargs['image'])
 
 
 class TopViewController(_ViewController):
@@ -343,127 +303,20 @@ class TopViewController(_ViewController):
     def btnClick_(_self, _cmd, _sender):
       this = ObjCInstance(_self)
       sender = ObjCInstance(_sender)
-      #pdbg.state(self.hp_switch)
-      hp_bool = self.hp_switch.isOn()
-      print(hp_bool)
-
-    @self.add_msg
-    def roughSetLayouts_(_self, _cmd, _views):
-      this = ObjCInstance(_self)
-      view = this.view()
-      layoutMarginsGuide = view.layoutMarginsGuide()
-
-      views = ObjCInstance(_views)
-
-      pre_child = None
-      for child in views:
-        view.addSubview_(child)
-
-        NSLayoutConstraint.activateConstraints_([
-          child.topAnchor().constraintEqualToAnchor_(pre_child.bottomAnchor())
-          if pre_child else child.topAnchor().constraintEqualToAnchor_(
-            view.topAnchor()),
-          child.leadingAnchor().constraintEqualToAnchor_(
-            layoutMarginsGuide.leadingAnchor()),
-          child.trailingAnchor().constraintEqualToAnchor_(
-            layoutMarginsGuide.trailingAnchor()),
-        ])
-
-        # xxx: ひでーけど、取り急ぎ(特定のclass かどうかを知りたい)
-        if str(child)[:12] == '<UIImageView':
-          NSLayoutConstraint.activateConstraints_([
-            child.widthAnchor().constraintEqualToConstant_(48.0),
-            child.heightAnchor().constraintEqualToConstant_(48.0),
-          ])
-        if str(child)[:13] == '<UIScrollView':
-          #pdbg.state(child)
-          NSLayoutConstraint.activateConstraints_([
-            child.widthAnchor().constraintEqualToAnchor_multiplier_(
-              layoutMarginsGuide.widthAnchor(), 1.0),
-            child.heightAnchor().constraintEqualToAnchor_multiplier_(
-              layoutMarginsGuide.widthAnchor(), 0.5),
-          ])
-
-        pre_child = child
 
   def didLoad(self, this: UIViewController):
     view = this.view()
     navigationItem = this.navigationItem()
     navigationItem.setTitle_(self.nav_title)
-
-    # --- view
     view.setBackgroundColor_(UIColor.systemBlueColor())
 
-    # xxx: 雑に並べていく
-    self.header_icon_img = UIImage.imageWithContentsOfFile_(
-      str(self.dummy_img_path))
-
-    self.header_icon = WrapImageView.new(image=self.header_icon_img)
-    self.header_icon.setContentMode_(scaleAspectFit)
-
-    self.header_label = WrapLabelView.new(text=self.nav_title)
+    # --- view
+    self.main_stack = ObjcStackView.new()
+    # --- header
+    self.header_stack = ObjcStackView.new()
     
-
-    self.uid_label = WrapLabelView.new(text='UID:')
-
-    placeholder = NSAttributedString.alloc().initWithString_('📝 ここに、UID を入力 🥺')
-    self.uid_textfield = WrapTextFieldView.new()
-    self.uid_textfield.setAttributedPlaceholder_(placeholder)
-
-    self.username_key_label = WrapLabelView.new(text='ユーザー名:')
-    self.username_value_label = WrapLabelView.new(text='ここにユーザー名が入る？😂')
-
-    self.worldrank_key_label = WrapLabelView.new(text='世界ランク:')
-    self.worldrank_value_label = WrapLabelView.new(text='ここに世界ランクが入る?😂')
-
-    self.result_scroll = WrapScrollView.new()
-    self.result_scroll.setContentSize_((0.0, 1024.0))
-    self.result_scroll.setBackgroundColor_(UIColor.systemRedColor())
-    #pdbg.state(self.result_scroll)
-
-    self.computation_view = WrapLabelView.new(text='計算方式')
-
-    self.hp_label = WrapLabelView.new(text='HP(換算):')
-    self.hp_switch = WrapSwitch.new()
-    self.power_label = WrapLabelView.new(text='攻撃力(換算):')
-    self.power_switch = WrapSwitch.new()
-    self.defence_label = WrapLabelView.new(text='防御(換算):')
-    self.defence_switch = WrapSwitch.new()
-    self.charge_label = WrapLabelView.new(text='元素チャージ効率(換算):')
-    self.charge_switch = WrapSwitch.new()
-
-    self.familiarity_label = WrapLabelView.new(text='熟知(換算):')
-    self.familiarity_switch = WrapSwitch.new()
-
-    self.btn = WrapButton.new(title='作成')
-    self.btn.addTarget_action_forControlEvents_(this, sel('btnClick:'), UIControlEventTouchUpInside)
-
-    # todo: layout
-    '''
-    this.roughSetLayouts_([
-      self.header_icon,
-      self.header_label,
-      self.uid_label,
-      self.uid_textfield,
-      self.username_key_label,
-      self.username_value_label,
-      self.worldrank_key_label,
-      self.worldrank_value_label,
-      self.result_scroll,
-      self.computation_view,
-      self.hp_label,
-      self.hp_switch,
-      self.power_label,
-      self.power_switch,
-      self.defence_label,
-      self.defence_switch,
-      self.charge_label,
-      self.charge_switch,
-      self.familiarity_label,
-      self.familiarity_switch,
-      self.btn,
-    ])
-    '''
+    
+    
 
 
 if __name__ == '__main__':
@@ -471,8 +324,4 @@ if __name__ == '__main__':
   fvc = TopViewController.new(name=top_name)
   nvc = NavigationController.new(fvc)
   present_objc(nvc)
-
-
-
-
 
