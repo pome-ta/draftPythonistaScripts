5c11788c6c699ec5da7eaf79fb098aa4aff1d465
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-01-10 13:30:32 +0900

    up

--- requestProjects/Artifacter/sandbox/240110_1304.py
+++ requestProjects/Artifacter/sandbox/240110_1304.py
@@ -4,4 +4,7 @@ import json
 json_path = Path('./data.json')
 #json_obj = json.loads(json_path.read_bytes())
 json_obj = json.loads(json_path.read_text(encoding='utf-8'))
-print(json.dumps(json_obj, ensure_ascii=False, indent=2))
+dump = json.dumps(json_obj, ensure_ascii=False, indent=2)
+#print(json.dumps(json_obj, ensure_ascii=False, indent=2))
+
+json_path.write_text(dump, encoding='utf-8')
