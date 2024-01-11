6eda5cc517c878b1deb0e3ab3a61cb32e20d68ce
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-01-11 09:11:18 +0900

    up

--- requestProjects/Artifacter/sandbox/240110_1943.py
+++ requestProjects/Artifacter/sandbox/240110_1943.py
@@ -1,21 +1,40 @@
-import concurrent.futures
+from pathlib import Path
+import json
+import urllib.request
+from concurrent.futures import ThreadPoolExecutor
+
+UsrAgn = 'Mozilla/5.0 (Linux; U; Android 8.0; en-la; Nexus Build/JPG991) AppleWebKit/511.2 (KHTML, like Gecko) Version/5.0 Mobile/11S444 YJApp-ANDROID jp.co.yahoo.android.yjtop/4.01.1.5'
 
 
 class Artifacter:
 
   def __init__(self, uid):
     self.uid = uid
-    
+    ThreadPoolExecutor().submit(self.Initialization).result()
+
   def Initialization(self):
-    pass
+    # データの表記名データをEnka.Network公式Githubから取得
+    #self.locale_jp = ThreadPoolExecutor().submit(self.locale).result()
+    # キャラクターデータをEnka.Network公式Githubから取得
+    #self.characters_json = ThreadPoolExecutor().submit(self.character_json).result()
+    #self.costumes = ThreadPoolExecutor().submit(self.costume_json).result()
+    
+    #try:
+    url = fr'https://enka.network/api/uid/{self.uid}'
+    req = urllib.request.Request(url, headers={'User-Agent': UsrAgn})
+    print('r')
+    print(req)
+    json_data = urllib.request.urlopen(req).read().decode(errors='ignore')
+    self.player_data = json.loads(json_data)
     
   def main(self):
     print('o')
 
 
 if __name__ == '__main__':
-  UID = 
-  artifacter = Artifacter(0)
+  uid_path = Path('./uid.txt')
+  UID = uid_path.read_text()
+  artifacter = Artifacter(UID)
   print('h')
-  concurrent.futures.ThreadPoolExecutor().submit(artifacter.main)
+  #ThreadPoolExecutor().submit(artifacter.main)
 
