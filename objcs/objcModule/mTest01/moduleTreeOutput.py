ace3cf4b168af10d1d94fc4459631155d618fdfd
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-01-27 16:10:07 +0900

    up

--- objcs/objcModule/mTest01/moduleTreeOutput.py
+++ objcs/objcModule/mTest01/moduleTreeOutput.py
@@ -1,11 +1,12 @@
 from pathlib import Path
+#from  glob import glob
 import clipboard
 
 
 module_root_uri = './objcista'
 module_root_path = Path(module_root_uri)
 
-for p in module_root_path.glob('**'):
+for p in module_root_path.iterdir():
   print(p)
-  if p.suffix  == '.icloud':
-    p.unlink()
+  
+
