a71e8a916e105035b2d8e6403c5acc51aa1b4686
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-01-27 21:18:12 +0900

    up

--- objcs/objcModule/mTest01/moduleTreeOutput.py
+++ objcs/objcModule/mTest01/moduleTreeOutput.py
@@ -2,11 +2,16 @@ from pathlib import Path
 #from  glob import glob
 import clipboard
 
-
 module_root_uri = './objcista'
 module_root_path = Path(module_root_uri)
 
-for p in module_root_path.iterdir():
-  print(p)
-  
+
+def gene_tree(tree_path: Path, tree_depth: int = 0, tree_str: str = '') -> str:
+  side_line = '|' + '─' * tree_depth + ' ' if tree_depth else None
+  is_dir = tree_path.is_dir()
+  tree_str += tree_path.name + ('/' if is_dir else None) + '\n'
+  return tree_str
+
+
+tree_path = gene_tree(module_root_path)
 
