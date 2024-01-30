1e8bf7742896c4674bc1105ca69f6a62a1a68ab2
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-01-30 22:13:18 +0900

    up

---
+++ objcs/uiview/viewTransition/240130_2207.py
@@ -0,0 +1,388 @@
+from objc_util import ObjCClass, ObjCInstance, create_objc_class, on_main_thread
+from objc_util import sel, CGRect
+
+import pdbg
+
+# --- navigation
+UINavigationController = ObjCClass('UINavigationController')
+UINavigationBarAppearance = ObjCClass('UINavigationBarAppearance')
+UIBarButtonItem = ObjCClass('UIBarButtonItem')
+
+# --- viewController
+UIViewController = ObjCClass('UIViewController')
+UISheetPresentationController = ObjCClass('UISheetPresentationController')
+
+
+class NavigationController:
+
+  def __init__(self):
+    self._navigationController: UINavigationController
+
+  def _override_navigationController(self):
+    # --- `UINavigationController` Methods
+    def doneButtonTapped_(_self, _cmd, _sender):
+      this = ObjCInstance(_self)
+      visibleViewController = this.visibleViewController()
+      visibleViewController.dismissViewControllerAnimated_completion_(
+        True, None)
+
+    # --- `UINavigationController` set up
+    _methods = [
+      doneButtonTapped_,
+    ]
+
+    create_kwargs = {
+      'name': '_nv',
+      'superclass': UINavigationController,
+      'methods': _methods,
+    }
+    _nv = create_objc_class(**create_kwargs)
+    self._navigationController = _nv
+
+  def create_navigationControllerDelegate(self):
+    # --- `UINavigationControllerDelegate` Methods
+    def navigationController_willShowViewController_animated_(
+        _self, _cmd, _navigationController, _viewController, _animated):
+
+      navigationController = ObjCInstance(_navigationController)
+      viewController = ObjCInstance(_viewController)
+
+      # --- appearance
+      appearance = UINavigationBarAppearance.alloc()
+      appearance.configureWithDefaultBackground()
+
+      # --- navigationBar
+      navigationBar = navigationController.navigationBar()
+
+      navigationBar.standardAppearance = appearance
+      navigationBar.scrollEdgeAppearance = appearance
+      navigationBar.compactAppearance = appearance
+      navigationBar.compactScrollEdgeAppearance = appearance
+
+      viewController.setEdgesForExtendedLayout_(0)
+
+      done_btn = UIBarButtonItem.alloc(
+      ).initWithBarButtonSystemItem_target_action_(0, navigationController,
+                                                   sel('doneButtonTapped:'))
+
+      visibleViewController = navigationController.visibleViewController()
+
+      # --- navigationItem
+      navigationItem = visibleViewController.navigationItem()
+
+      navigationItem.rightBarButtonItem = done_btn
+
+    # --- `UINavigationControllerDelegate` set up
+    _methods = [
+      navigationController_willShowViewController_animated_,
+    ]
+    _protocols = [
+      'UINavigationControllerDelegate',
+    ]
+
+    create_kwargs = {
+      'name': '_nvDelegate',
+      'methods': _methods,
+      'protocols': _protocols,
+    }
+    _nvDelegate = create_objc_class(**create_kwargs)
+    return _nvDelegate.new()
+
+  @on_main_thread
+  def _init(self, vc: UIViewController):
+    self._override_navigationController()
+    _delegate = self.create_navigationControllerDelegate()
+    nv = self._navigationController.alloc()
+    nv.initWithRootViewController_(vc).autorelease()
+    nv.setDelegate_(_delegate)
+    return nv
+
+  @classmethod
+  def new(cls, vc: UIViewController) -> ObjCInstance:
+    _cls = cls()
+    return _cls._init(vc)
+
+
+class _ViewController:
+
+  def __init__(self, *args, **kwargs):
+    self._msgs: list['def'] = []  # xxx: 型ちゃんとやる
+    self._viewController: UIViewController
+    self.override()
+
+  def override(self):
+    # todo: objc で特別にmethod 生やしたいときなど
+    pass
+
+  def add_msg(self, msg):
+    if not (hasattr(self, '_msgs')):
+      self._msgs: list['def'] = []
+    self._msgs.append(msg)
+
+  def didLoad(self, this: UIViewController):
+    pass
+
+  def willAppear(self, this: UIViewController, animated: bool):
+    pass
+
+  def willLayoutSubviews(self, this: UIViewController):
+    pass
+
+  def didLayoutSubviews(self, this: UIViewController):
+    pass
+
+  def didAppear(self, this: UIViewController, animated: bool):
+    pass
+
+  def willDisappear(self, this: UIViewController, animated: bool):
+    pass
+
+  def didDisappear(self, this: UIViewController, animated: bool):
+    pass
+
+  def _override_viewController(self):
+    # --- `UIViewController` Methods
+    def viewDidLoad(_self, _cmd):
+      this = ObjCInstance(_self)
+      self.didLoad(this)
+
+    def viewWillAppear_(_self, _cmd, animated):
+      this = ObjCInstance(_self)
+      self.willAppear(this, animated)
+
+    def viewWillLayoutSubviews(_self, _cmd):
+      this = ObjCInstance(_self)
+      self.willLayoutSubviews(this)
+
+    def viewDidLayoutSubviews(_self, _cmd):
+      this = ObjCInstance(_self)
+      self.didLayoutSubviews(this)
+
+    def viewDidAppear_(_self, _cmd, animated):
+      this = ObjCInstance(_self)
+      self.didAppear(this, animated)
+
+    def viewWillDisappear_(_self, _cmd, animated):
+      this = ObjCInstance(_self)
+      self.willDisappear(this, animated)
+
+    def viewDidDisappear_(_self, _cmd, animated):
+      this = ObjCInstance(_self)
+      self.didDisappear(this, animated)
+
+    # --- `UIViewController` set up
+    _methods = [
+      viewDidLoad,
+      viewWillAppear_,
+      viewWillLayoutSubviews,
+      viewDidLayoutSubviews,
+      viewDidAppear_,
+      viewWillDisappear_,
+      viewDidDisappear_,
+    ]
+
+    if self._msgs: _methods.extend(self._msgs)
+
+    create_kwargs = {
+      'name': '_vc',
+      'superclass': UIViewController,
+      'methods': _methods,
+    }
+    _vc = create_objc_class(**create_kwargs)
+    self._viewController = _vc
+
+  def _init(self):
+    self._override_viewController()
+    vc = self._viewController.new().autorelease()
+    return vc
+
+  @classmethod
+  def new(cls, *args, **kwargs) -> ObjCInstance:
+    _cls = cls(*args, **kwargs)
+    return _cls._init()
+
+
+@on_main_thread
+def present_objc(vc):
+  app = ObjCClass('UIApplication').sharedApplication()
+  window = app.keyWindow() if app.keyWindow() else app.windows().firstObject()
+
+  root_vc = window.rootViewController()
+
+  while root_vc.presentedViewController():
+    root_vc = root_vc.presentedViewController()
+
+  vc.setModalPresentationStyle(0)
+  root_vc.presentViewController_animated_completion_(vc, True, None)
+
+
+### ### ###
+# --- view
+UIView = ObjCClass('UIView')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+UIColor = ObjCClass('UIColor')
+UIButton = ObjCClass('UIButton')
+UIButtonConfiguration = ObjCClass('UIButtonConfiguration')
+UIControlEventTouchUpInside = 1 << 6
+
+
+class FirstViewController(_ViewController):
+
+  def __init__(self):
+    super().__init__()
+    self.nav_title = 'FirstViewController'
+    self.sub_view = UIView.alloc()
+    self.btn = UIButton.new()
+
+  def override(self):
+
+    @self.add_msg
+    def btnClick_(_self, _cmd, _sender):
+      this = ObjCInstance(_self)
+      sender = ObjCInstance(_sender)
+      svc = SecondViewController.new(name='SecondViewController')
+      navigationController = this.navigationController()
+      navigationController.pushViewController_animated_(svc, True)
+
+  def didLoad(self, this: UIViewController):
+    view = this.view()
+    view.setBackgroundColor_(UIColor.systemBlueColor())
+
+    navigationItem = this.navigationItem()
+    navigationItem.setTitle_(self.nav_title)
+
+    # --- view
+    config = UIButtonConfiguration.tintedButtonConfiguration()
+    config.setTitle_('tap')
+    config.setBaseBackgroundColor_(UIColor.systemPinkColor())
+    config.setBaseForegroundColor_(UIColor.systemGreenColor())
+
+    self.btn.setConfiguration_(config)
+
+    self.btn.addTarget_action_forControlEvents_(this, sel('btnClick:'),
+                                                UIControlEventTouchUpInside)
+
+    # --- layout
+    view.addSubview_(self.btn)
+
+    self.btn.translatesAutoresizingMaskIntoConstraints = False
+
+    NSLayoutConstraint.activateConstraints_([
+      self.btn.centerXAnchor().constraintEqualToAnchor_(view.centerXAnchor()),
+      self.btn.centerYAnchor().constraintEqualToAnchor_(view.centerYAnchor()),
+      self.btn.widthAnchor().constraintEqualToAnchor_multiplier_(
+        view.widthAnchor(), 0.4),
+      self.btn.heightAnchor().constraintEqualToAnchor_multiplier_(
+        view.heightAnchor(), 0.1),
+    ])
+
+
+class SecondViewController(_ViewController):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.nav_title = kwargs['name']
+    self.sub_view = UIView.alloc()
+    self.btn = UIButton.new()
+
+  def override(self):
+
+    @self.add_msg
+    def btnClick_(_self, _cmd, _sender):
+      this = ObjCInstance(_self)
+      sender = ObjCInstance(_sender)
+      tvc = ThirdViewController.new()
+      navigationController = this.navigationController()
+      navigationController.pushViewController_animated_(tvc, True)
+
+  def didLoad(self, this: UIViewController):
+    view = this.view()
+    view.setBackgroundColor_(UIColor.systemGreenColor())
+
+    navigationItem = this.navigationItem()
+    navigationItem.setTitle_(self.nav_title)
+
+    # --- view
+    config = UIButtonConfiguration.tintedButtonConfiguration()
+    config.setTitle_('tap')
+    config.setBaseBackgroundColor_(UIColor.systemPinkColor())
+    config.setBaseForegroundColor_(UIColor.systemBlueColor())
+
+    self.btn.setConfiguration_(config)
+
+    self.btn.addTarget_action_forControlEvents_(this, sel('btnClick:'),
+                                                UIControlEventTouchUpInside)
+
+    # --- layout
+    view.addSubview_(self.btn)
+
+    self.btn.translatesAutoresizingMaskIntoConstraints = False
+
+    NSLayoutConstraint.activateConstraints_([
+      self.btn.centerXAnchor().constraintEqualToAnchor_(view.centerXAnchor()),
+      self.btn.centerYAnchor().constraintEqualToAnchor_(view.centerYAnchor()),
+      self.btn.widthAnchor().constraintEqualToAnchor_multiplier_(
+        view.widthAnchor(), 0.4),
+      self.btn.heightAnchor().constraintEqualToAnchor_multiplier_(
+        view.heightAnchor(), 0.1),
+    ])
+
+
+class ThirdViewController(_ViewController):
+
+  def __init__(self):
+    super().__init__()
+    self.nav_title = 'ThirdViewController'
+    self.sub_view = UIView.alloc()
+    self.btn = UIButton.new()
+
+  def override(self):
+
+    def btnClick_(_self, _cmd, _sender):
+      this = ObjCInstance(_self)
+      navigationController = this.navigationController()
+      visibleViewController = navigationController.visibleViewController()
+      visibleViewController.dismissViewControllerAnimated_completion_(
+        True, None)
+
+    self.add_msg(btnClick_)
+
+  def didLoad(self, this: UIViewController):
+    view = this.view()
+    view.setBackgroundColor_(UIColor.systemPinkColor())
+
+    navigationItem = this.navigationItem()
+    navigationItem.setTitle_(self.nav_title)
+
+    # --- view
+    config = UIButtonConfiguration.tintedButtonConfiguration()
+    config.setTitle_('tap')
+    config.setBaseBackgroundColor_(UIColor.systemGreenColor())
+    config.setBaseForegroundColor_(UIColor.systemPinkColor())
+
+    self.btn.setConfiguration_(config)
+
+    self.btn.addTarget_action_forControlEvents_(this, sel('btnClick:'),
+                                                UIControlEventTouchUpInside)
+
+    # --- layout
+    view.addSubview_(self.btn)
+
+    self.btn.translatesAutoresizingMaskIntoConstraints = False
+
+    NSLayoutConstraint.activateConstraints_([
+      self.btn.centerXAnchor().constraintEqualToAnchor_(view.centerXAnchor()),
+      self.btn.centerYAnchor().constraintEqualToAnchor_(view.centerYAnchor()),
+      self.btn.widthAnchor().constraintEqualToAnchor_multiplier_(
+        view.widthAnchor(), 0.4),
+      self.btn.heightAnchor().constraintEqualToAnchor_multiplier_(
+        view.heightAnchor(), 0.1),
+    ])
+
+
+if __name__ == '__main__':
+  fvc = FirstViewController.new()
+  nvc = NavigationController.new(fvc)
+  present_objc(nvc)
+
