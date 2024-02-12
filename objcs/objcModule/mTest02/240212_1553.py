fb24109f42886f181e260cb4b9a52332f8ac8d35
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-02-12 16:07:08 +0900

    up

---
+++ objcs/objcModule/mTest02/240212_1553.py
@@ -0,0 +1,29 @@
+from objc_util import ObjCInstance, create_objc_class,class_getSuperclass,objc_getClass,objc_registerClassPair,objc_allocateClassPair
+
+from objcista import *
+import pdbg
+
+
+def viewDidLoad(_self, _cmd):
+  this = ObjCInstance(_self)
+  #pdbg.state(this)
+  _allocateClassPair = objc_allocateClassPair(_self, 'vc', 0)
+  #sc = objc_getClass(objc_registerClassPair(_self))
+  #print(sc)
+
+
+_methods = [
+  viewDidLoad,
+]
+create_kwargs = {
+  'name': 'vc',
+  'superclass': UIViewController,
+  'methods': _methods,
+}
+
+if __name__ == "__main__":
+  vc = create_objc_class(**create_kwargs)
+
+  style = UIModalPresentationStyle.pageSheet
+  run_controller(vc.new(), style)
+
