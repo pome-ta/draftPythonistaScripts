f26693d3cf291fc2d5ff7efbdddcfe4a34fe3afa
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-03-10 15:42:43 +0900

    rubicon test

---
+++ objcs/blockTest/rubiconTest/240310_1528.py
@@ -0,0 +1,7 @@
+from objc import ObjCClass
+
+import pdbg
+
+app = ObjCClass('UIApplication')
+
+pdbg.state(app)
---
+++ objcs/blockTest/rubiconTest/240310_1541.py
@@ -0,0 +1,7 @@
+from objc_util import ObjCClass
+import pdbg
+
+
+app = ObjCClass('UIApplication')
+
+pdbg.state(app)
