e3cde2db0505a980f46983017d52cbfc1d69acf4
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-04-25 11:31:38 +0900

    up

--- glsl/playGround/textureInOrb/240425_0230.js
+++ glsl/playGround/textureInOrb/240425_0230.js
@@ -12,13 +12,31 @@ const float PI = acos(-1.0);
 
 void main(){
   vec2 uv = v_tex_coord;
-  uv.x += sin(u_time);
+  //float diff = -sin(u_time) / 10.0;
+  float diff = 0.05;
+  vec4 texU = texture2D(u_texture, vec2(uv.x, uv.y - diff));
+  vec4 texD = texture2D(u_texture, vec2(uv.x, uv.y + diff));
+  vec4 texR = texture2D(u_texture, vec2(uv.x - diff, uv.y));
+  vec4 texL = texture2D(u_texture, vec2(uv.x + diff, uv.y));
+  
+  float max1 = max(texU.a, texD.a);
+  float max2 = max(texR.a, texL.a);
+  float maxFull = max(max1, max2);
+  
+  vec4 wrap = vec4(vec3(1.0, 0.5, 1.0), maxFull);
+  vec4 main = texture2D(u_texture, uv);
+  vec4 tex = mix(wrap,main, 0.1);
   
   
   
-  if (uv.x < 0.0 || uv.x > 1.0 || uv.y < 0.0 || uv.y > 1.0)discard;
+  
+  //if (uv.x < 0.0 || uv.x > 1.0 || uv.y < 0.0 || uv.y > 1.0)discard;
   //gl_FragColor = vec4(uv.x, uv.y, 0.0, 1.0);
-  gl_FragColor = texture2D(u_texture, uv);
+  //gl_FragColor = texture2D(u_texture, uv);
+  //gl_FragColor = vec4(vec3(maxFull), 1.0);
+  //gl_FragColor = main;
+  gl_FragColor = tex;
+  
   
 }
 
