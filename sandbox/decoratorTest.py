b02f24337b499b1acc5b08c2495cb6d2697bf88f
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-05-16 10:13:35 +0900

    for uikit challenge

--- sandbox/decoratorTest.py
+++ sandbox/decoratorTest.py
@@ -3,7 +3,6 @@
 # xxx: 変数が存在しない場合、生成できないのでは？
 
 fnc_list = []
-
 '''
 def add_fnc(fnc):
   def a(h):
@@ -16,21 +15,41 @@ def add_fnc(fnc):
 '''
 
 
+def _create_reuse_dict(cellClass: 'UITableViewCell', identifier: str) -> dict:
+  return {
+    'cellClass': cellClass,
+    'identifier': identifier,
+  }
+
+
+prototypes = []
+
+
+def add_prototype(identifier: str):
+
+  def wrapper(cellClass: 'UITableViewCell'):
+    prototypes.append(_create_reuse_dict(cellClass, identifier))
+
+  return wrapper
+
+
 def add_fnc(fnc):
+
   def a(h):
     #print(h)
     #print('a')
-    fnc_list.append({'hoge':h, 'fuga':fnc})
+    fnc_list.append({'hoge': h, 'fuga': fnc})
+
   #print(a,fnc)
   return a
 
 
-
-@add_fnc('higeeeeee')
+@add_prototype('higeeeeee')
 def fnc01():
   pass
 
-#@add_fnc('fuga')
+
+@add_prototype('aaaaa')
 def fnc02():
   pass
 
