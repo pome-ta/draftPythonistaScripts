043a88ca6555ac66df5e05bd467b0e5ac12ed14b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-07-14 14:27:42 +0900

    up

--- objcs/fileDirs/sandbox/240713_1909.py
+++ objcs/fileDirs/sandbox/240713_1909.py
@@ -1,12 +1,28 @@
+import webbrowser
+import ctypes
 from pathlib import Path
 
-from objc_util import ObjCClass
+from objc_util import ObjCClass, ObjCInstance, c
 
 import pdbg
 
-
 NSFileManager = ObjCClass('NSFileManager')
 
-pdbg.state(NSFileManager.defaultManager())
-#print(NSFileManager.defaultManager().temporaryDirectory().uri())
-#print(NSFileManager.new().temporaryDirectory().uri())
+
+def NSSearchPathForDirectoriesInDomains(directory, domainMask, expandTilde):
+  fnc = c.NSSearchPathForDirectoriesInDomains
+  fnc.restype = ctypes.c_void_p
+  fnc.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p]
+  return ObjCInstance(fnc(directory, domainMask, expandTilde))
+
+def get_toplevel_path(up_level: int=9) -> str:
+  origin_path = './'
+  parent_level = lambda l: '../' * int(l)
+  return origin_path + parent_level(up_level)
+
+
+libraryPath = NSSearchPathForDirectoriesInDomains(5, 1, True).firstObject()
+
+path = get_toplevel_path() + str(libraryPath)
+#print(path)
+webbrowser.open(f'pythonista3://{path}')
