b5593a70111081a59c10dfdab5bd4c05d28bd24d
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-07-16 18:58:13 +0900

    up

--- sceneUi/graphics/240716_1233.py
+++ sceneUi/graphics/240716_1233.py
@@ -37,7 +37,6 @@ class MainView(ui.View):
     self.scene_view.width = self.width
     self.scene_view.height = self.height * self.height_ratio
     self.scene_view.x = self.width / 2 - self.scene_view.width / 2
-    
 
   def setup_scene(self):
     scene_view = scene.SceneView()
@@ -45,7 +44,7 @@ class MainView(ui.View):
     scene_view.shows_fps = True
     scene_view.alpha = 0
     scene_view.scene = self.canvas
-    
+
     self.add_subview(scene_view)
     self.scene_view = scene_view
     self.layout()
@@ -80,31 +79,48 @@ class Canvas(scene.Scene):
 
   @ui.in_background
   def setup(self):
+    #print(self)
     div_count: int = 16
     w, h = self.size
-    self.bg_node = scene.Node()
-    #self.bg_node.position = [w / 2, h / 2]
-    self.add_child(self.bg_node)
+    self.cells_node = scene.Node()
+    self.add_child(self.cells_node)
+    
+    #self.cells_node.point_from_scene = (1.0, 1.0)
+    #self.cells_node.position = [w / 2, h / 2]
+    print(self.cells_node.point_to_scene((w / 2, h / 2)))
+    print(w / 2, h / 2)
+    #self.cells_node.position = [w / 2, h / 2]
+    #self.cells_node.point_from_scene((w / 2, h / 2))
+    
+    #self.cells_node.position = [0.0, 0.0]
     
 
     sq_size = int(min([w, h]) / 1.1)
-    
-    bg_rect = ui.Path.rect(0, 0, sq_size, sq_size)
-    self.bg_shapeNode = scene.ShapeNode(path=bg_rect,fill_color=0.5,parent=self.bg_node)
-    #self.bg_shapeNode.anchor_point=(0.0, 0.0)
+    path_rect = ui.Path.rect(0.0,0.0,sq_size,sq_size)
+    self.bg = scene.ShapeNode(path=path_rect,parent=self)
+    self.bg.position = [w / 2, h / 2]
+    #self.bg.anchor_point=(1.0,1.0)
     
 
     cell_size = sq_size / div_count
-    for x_i,x_w in enumerate(range(div_count)):
-      color = hsv_to_rgb(x_i/div_count, 1.0, 1.0)
-      
-      
-      cell_rect = ui.Path.rect(0, 0, cell_size, cell_size)
-      self.cell = scene.ShapeNode(path=cell_rect, fill_color=color,parent=self.bg_shapeNode)
-      self.cell.position = (cell_size*x_i, 0.0)
-    #self.cell.
 
-  @ui.in_background
+    self.cells_list = [[
+      self.set_cells(cell_size, self.bg, div_count, x, y)
+      for x in range(div_count)
+    ] for y in range(div_count)]
+
+  def set_cells(self, cell_size, parent, div_count, x, y):
+    h = (x + (y * div_count)) / (div_count * div_count)
+    fill_color = hsv_to_rgb(h, 1.0, 1.0)
+    cell_rect = ui.Path.rect(0, 0, cell_size, cell_size)
+    cell = scene.ShapeNode(path=cell_rect,
+                           fill_color=fill_color,
+                           parent=parent)
+    #cell.anchor_point = (0.0, 0.0)
+    cell.position = (cell_size * x, cell_size * y)
+    return cell
+
+  #@ui.in_background
   def update(self):
     #print(f'{self.t}')  # 画面左下にlog として表示される
     pass
@@ -119,13 +135,12 @@ if __name__ == '__main__':
   AF_COLOR = TINT_COLOR
 
   canvas = Canvas()
-  
 
   view = MainView(canvas)
   view.present(style='fullscreen',
                orientations='portrait',
                title_bar_color=TINT_COLOR,
                title_color=TITLE_COLOR)
-  
+
   #scene.run(canvas)
-  
+
