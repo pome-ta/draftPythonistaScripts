9aeffb922765388ce1729d8d3ab52d08992862ed
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-07-17 01:43:14 +0900

    up

---
+++ graphics/opticalIllusion/240717_0119.py
@@ -0,0 +1,41 @@
+from colorsys import hsv_to_rgb
+
+import ui
+
+class BoardView(ui.View):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.bg_color = 'maroon'
+
+
+
+class MainView(ui.View):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.bg_color = 'maroon'
+    #self.update_interval = 1 / 60
+
+    self.div_num = 16
+
+  def will_close(self):
+    pass
+
+  def draw(self):
+    pass
+
+  def update(self):
+    print('update')
+    self.set_needs_display()
+
+  def layout(self):
+    pass
+
+
+if __name__ == '__main__':
+  view = MainView()
+  #view.present()
+  #view.present(hide_title_bar=True)
+  view.present(style='fullscreen', orientations=['portrait'])
+
