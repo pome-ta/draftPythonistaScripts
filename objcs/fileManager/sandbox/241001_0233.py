d5eff1fa37207d236d9523049c495baf7e153514
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-10-01 12:02:02 +0900

    sound path

---
+++ objcs/fileManager/sandbox/241001_0233.py
@@ -0,0 +1,30 @@
+"""
+note: file のmeta 情報
+"""
+
+from pathlib import Path
+
+from objc_util import ObjCClass
+import pdbg
+
+NSFileManager = ObjCClass('NSFileManager')
+
+#attributesOfItemAtPath_error_
+#attributesOfItemAtPath_error_
+#pdbg.state(NSFileManager.defaultManager())
+
+root_str = '/System/Library/Audio/UISounds/'
+root_path = Path(root_str)
+
+all_files = list(root_path.glob('**/*.*'))
+
+#sound_path = all_files
+'''
+for sound_path in all_files:
+  attributes = NSFileManager.defaultManager().attributesOfItemAtPath_error_(
+    str(sound_path), None)
+'''
+
+
+
+#attributes = [NSFileManager.defaultManager().attributesOfItemAtPath_error_(str(sound_path), None) for sound_path in all_files]
