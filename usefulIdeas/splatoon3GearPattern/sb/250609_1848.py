51d26835cef6d804864649b99c9cdfc3a6ea721d
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-06-10 00:07:35 +0900

    up

--- usefulIdeas/splatoon3GearPattern/sb/250609_1848.py
+++ usefulIdeas/splatoon3GearPattern/sb/250609_1848.py
@@ -1,22 +1,13 @@
 from itertools import product
 
 
-def create_pattern(
-  main_gears: list[str], sub_gear_patterns: list[list[str, str, str, ]]
-) -> list[list[str, list[str, str, str, ]]]:
-  pass
-
-
-def create_sub_gear_patterns(
-    sub_gears: list[str]) -> list[list[str, str, str, ]]:
-  #return list(map(list, set(map(tuple, map(sorted, product(normal_gears, repeat=3))))))
-  product_iter = product(sub_gears, repeat=3)
-  sorted_tuple_map  = map(tuple, map(sorted, product_iter))
-  set_list = list(map(list, set(sorted_tuple_map)))
-  return set_list
-
-
 normal_gears = [  # 通常ギアパワー
+  'メ',  # インク効率アップ(メイン)
+  'サ',  # インク効率アップ(サブ)
+  '回',  # インク回復力アップ
+]
+
+_normal_gears = [  # 通常ギアパワー
   'メ',  # インク効率アップ(メイン)
   'サ',  # インク効率アップ(サブ)
   '回',  # インク回復力アップ
@@ -53,17 +44,38 @@ shoes_gears = [  # クツ専用ギアパワー
   '受',  # 受け身術
 ]
 
-# 2744
-#sub_gear_pattern = list(product(normal_gears,repeat=3))
 
-#sub_gear_pattern = set(list(map(sorted, product(normal_gears,repeat=3))))
+def create_pattern(
+  main_gears: list[str], sub_gear_patterns: list[list[str, str, str, ]]
+) -> list[list[str, list[str, str, str, ]]]:
+  product_iter = product(main_gears, sub_gear_patterns)
+  pattern_list = list(map(list, product_iter))
+  return pattern_list
 
-#sub_gear_pattern = list(set([tuple(sg) for sg in map(sorted, product(normal_gears,repeat=3))]))
 
-sub_gear_pattern1 = list(
-  map(list, set(map(tuple, map(sorted, product(normal_gears, repeat=3))))))
+def create_sub_gear_patterns(
+    sub_gears: list[str]) -> list[list[str, str, str, ]]:
+  #return list(map(list, set(map(tuple, map(sorted, product(normal_gears, repeat=3))))))
+  gears_add_num = [f'{n:02}{g}' for n, g in enumerate(sub_gears)]
 
-sub_gear_pattern2 = create_sub_gear_patterns(normal_gears)
-print(len(sub_gear_pattern1))
-print(len(sub_gear_pattern2))
+  #product_iter = product(sub_gears, repeat=3)
+  product_iter = product(gears_add_num, repeat=3)
+  sorted_tuple_map = map(tuple, map(sorted, product_iter))
+  set_list = {items for items in sorted_tuple_map}
+  print(set_list)
+  
+  #set_list = list(map(list, set(sorted_tuple_map)))
+  #print(len(set_list))
+  sorted_list = [list(x) for x in (zip(*[sorted(l) for l in zip(*set_list)]))]
+  
+  
+  #gears_remove_num = 
+  
+  #return set_list
+  return sorted_list
+
+
+sub_patterns = create_sub_gear_patterns(normal_gears)
+
+m = create_pattern(normal_gears, sub_patterns)
 
