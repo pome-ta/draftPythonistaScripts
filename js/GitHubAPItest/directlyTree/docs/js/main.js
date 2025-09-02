2246de48d8c1441218cf05f7ad26e4ccfe3923ef
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-09-02 15:14:04 +0900

    up

--- js/GitHubAPItest/directlyTree/docs/js/main.js
+++ js/GitHubAPItest/directlyTree/docs/js/main.js
@@ -1,3 +1,19 @@
 import DomFactory from './utils/domFactory.js';
 
 
+
+const owner = 'pome-ta';
+const repo = 'p5SketchBook'
+
+const endPoint = 'https://api.github.com/repos';
+const kind = 'contents'
+
+
+
+async function getRepoContents() {
+  const res = await fetch([endPoint, owner, repo, kind].join('/'));
+  const data = await res.json();
+  console.log(data);
+}
+
+getRepoContents();
