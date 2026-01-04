6ee8c960cddbca1594d1bccdf0b1be2437bb9209
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-01-04 14:52:37 +0900

    debug?

--- objcs/ARKit/rubiconARKit/__rubiconARKtiView__.py
+++ objcs/ARKit/rubiconARKit/__rubiconARKtiView__.py
@@ -3,11 +3,15 @@ import ctypes
 from pyrubicon.objc.api import ObjCClass
 from pyrubicon.objc.api import objc_method
 from pyrubicon.objc.runtime import send_super
+from pyrubicon.objc.types import CGRectMake
 
 from rbedge.functions import NSStringFromClass
 from rbedge import pdbr
 
+SCNView = ObjCClass('SCNView')
+
 UIViewController = ObjCClass('UIViewController')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
 
 
 class MainViewController(UIViewController):
@@ -15,19 +19,54 @@ class MainViewController(UIViewController):
   @objc_method
   def dealloc(self):
     # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
-    print(f'\t - {NSStringFromClass(__class__)}: dealloc')
+    #print(f'\t - {NSStringFromClass(__class__)}: dealloc')
+    pass
 
   @objc_method
   def loadView(self):
     send_super(__class__, self, 'loadView')
-    print(f'\t{NSStringFromClass(__class__)}: loadView')
 
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')
-    print(f'\t{NSStringFromClass(__class__)}: viewDidLoad')
     self.navigationItem.title = NSStringFromClass(__class__)
-    pdbr.state(self.view)
+
+    #scnView = SCNView.new()
+    scnView = SCNView.alloc().initWithFrame_(CGRectMake(
+      0.0, 0.0, 100.0, 100.0))
+    scnView.setShowsStatistics_(True)
+    #pdbr.state(scnView)
+    # --- Layout
+    safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
+    pdbr.state(scnView.leadingAnchor)
+
+    self.view.addSubview_(scnView)
+    scnView.translatesAutoresizingMaskIntoConstraints = False
+    '''
+    
+    NSLayoutConstraint.activateConstraints_([
+      scnView.topAnchor.constraintEqualToAnchor_(
+        safeAreaLayoutGuide.topAnchor),
+      scnView.leadingAnchor.constraintEqualToAnchor_(safeAreaLayoutGuide.leadingAnchor),
+      #scnView.leadingAnchor.constraintEqualToAnchor_multiplier_(safeAreaLayoutGuide.leadingAnchor,0.8),
+      scnView.trailingAnchor.constraintEqualToAnchor_(
+        safeAreaLayoutGuide.trailingAnchor),
+      scnView.bottomAnchor.constraintEqualToAnchor_(safeAreaLayoutGuide.bottomAnchor),
+      
+    ])
+    '''
+    
+    NSLayoutConstraint.activateConstraints_([
+      scnView.centerXAnchor.constraintEqualToAnchor_(
+        safeAreaLayoutGuide.centerXAnchor),
+      scnView.centerYAnchor.constraintEqualToAnchor_(
+        safeAreaLayoutGuide.centerYAnchor),
+      scnView.widthAnchor.constraintEqualToAnchor_multiplier_(
+        safeAreaLayoutGuide.widthAnchor, 1.0),
+      scnView.heightAnchor.constraintEqualToAnchor_multiplier_(
+        safeAreaLayoutGuide.heightAnchor, 0.5),
+    ])
+
 
   @objc_method
   def viewWillAppear_(self, animated: bool):
@@ -38,7 +77,6 @@ class MainViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    print(f'\t{NSStringFromClass(__class__)}: viewWillAppear_')
 
   @objc_method
   def viewDidAppear_(self, animated: bool):
@@ -49,12 +87,9 @@ class MainViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    print(f'\t{NSStringFromClass(__class__)}: viewDidAppear_')
-    print('\t↓ ---')
 
   @objc_method
   def viewWillDisappear_(self, animated: bool):
-    print('\t↑ ---')
     send_super(__class__,
                self,
                'viewWillDisappear:',
@@ -62,7 +97,6 @@ class MainViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    print(f'\t{NSStringFromClass(__class__)}: viewWillDisappear_')
 
   @objc_method
   def viewDidDisappear_(self, animated: bool):
@@ -73,7 +107,6 @@ class MainViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    print(f'\t{NSStringFromClass(__class__)}: viewDidDisappear_')
 
   @objc_method
   def didReceiveMemoryWarning(self):
@@ -85,13 +118,13 @@ if __name__ == '__main__':
   from rbedge.app import App
   from rbedge.enumerations import UIModalPresentationStyle
 
-  print('--- run ---')
+  #print('--- run ---')
   main_vc = MainViewController.new()
 
-  #presentation_style = UIModalPresentationStyle.fullScreen
-  presentation_style = UIModalPresentationStyle.pageSheet
+  presentation_style = UIModalPresentationStyle.fullScreen
+  #presentation_style = UIModalPresentationStyle.pageSheet
 
   app = App(main_vc, presentation_style)
   app.present()
-  print('--- end ---')
+  #print('--- end ---')
 
