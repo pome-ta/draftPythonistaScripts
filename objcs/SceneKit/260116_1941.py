68bdbb92aabc6dd8a9bd8a3bc1db704d1f0b7bd0
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-01-17 12:56:25 +0900

    up

---
+++ objcs/SceneKit/260116_1941.py
@@ -0,0 +1,161 @@
+#https://cdn-ak.f.st-hatena.com/images/fotolife/x/x67x6fx74x6f/20080922/20080922100958.jpg
+# [iOS で SceneKit を試す(Swift 3) その4 - SceneKit の構造 - Apple Engine](https://appleengine.hatenablog.com/entry/2017/05/30/192435)
+
+import ctypes
+from enum import IntEnum, IntFlag
+
+from pyrubicon.objc.api import ObjCClass, ObjCInstance
+from pyrubicon.objc.api import objc_method, objc_property, objc_const
+from pyrubicon.objc.runtime import send_super, load_library
+from pyrubicon.objc.types import CGRect, NSUInteger
+
+from rbedge.functions import NSStringFromClass
+from rbedge import pdbr
+
+SCNScene = ObjCClass('SCNScene')
+SCNView = ObjCClass('SCNView')
+
+UIViewController = ObjCClass('UIViewController')
+UIColor = ObjCClass('UIColor')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+
+class SCNDebugOptions(IntFlag):
+  none = 0
+  showPhysicsShapes = 1 << 0
+  showBoundingBoxes = 1 << 1
+  showLightInfluences = 1 << 2
+  showLightExtents = 1 << 3
+  showPhysicsFields = 1 << 4
+  showWireframe = 1 << 5
+  renderAsWireframe = 1 << 6
+  showSkeletons = 1 << 7
+  showCreases = 1 << 8
+  showConstraints = 1 << 9
+  showCameras = 1 << 10
+
+
+class GameScene:
+
+  def __init__(self):
+    self.scene: SCNScene
+    self.setUpScene()
+
+  def setUpScene(self):
+    scene = SCNScene.scene()
+    # ---
+    # ここに処理を書いていく
+    # ---
+    self.scene = scene
+
+
+class MainViewController(UIViewController):
+
+  scnView: SCNView = objc_property()
+
+  @objc_method
+  def dealloc(self):
+    # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
+    #print(f'\t - {NSStringFromClass(__class__)}: dealloc')
+    pass
+
+  @objc_method
+  def loadView(self):
+    send_super(__class__, self, 'loadView')
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    self.navigationItem.title = NSStringFromClass(__class__)
+
+    gameScene = GameScene()
+    
+    scnView = SCNView.new()
+    scnView.setScene_(gameScene.scene)
+    
+    
+
+    debugOptions = SCNDebugOptions.showCameras
+    scnView.setDebugOptions_(debugOptions)
+    scnView.setShowsStatistics_(True)
+    scnView.setAllowsCameraControl_(True)
+
+    self.view.addSubview_(scnView)
+
+    # --- Layout
+
+    scnView.translatesAutoresizingMaskIntoConstraints = False
+
+    safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
+
+    NSLayoutConstraint.activateConstraints_([
+      scnView.centerXAnchor.constraintEqualToAnchor_(
+        safeAreaLayoutGuide.centerXAnchor),
+      scnView.centerYAnchor.constraintEqualToAnchor_(
+        safeAreaLayoutGuide.centerYAnchor),
+      scnView.widthAnchor.constraintEqualToAnchor_multiplier_(
+        safeAreaLayoutGuide.widthAnchor, 1.0),
+      scnView.heightAnchor.constraintEqualToAnchor_multiplier_(
+        safeAreaLayoutGuide.heightAnchor, 1.0),
+    ])
+
+    self.scnView = scnView
+
+  @objc_method
+  def viewWillAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def viewWillDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def viewDidDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def didReceiveMemoryWarning(self):
+    send_super(__class__, self, 'didReceiveMemoryWarning')
+    print(f'\t{NSStringFromClass(__class__)}: didReceiveMemoryWarning')
+
+
+if __name__ == '__main__':
+  from rbedge.app import App
+  from rbedge.enumerations import UIModalPresentationStyle
+
+  main_vc = MainViewController.new()
+
+  presentation_style = UIModalPresentationStyle.fullScreen
+  #presentation_style = UIModalPresentationStyle.pageSheet
+
+  app = App(main_vc, presentation_style)
+  app.present()
+
