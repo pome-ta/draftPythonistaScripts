3c1349389b1d28faf83083ad2a1f02b8bc442068
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-02-24 18:36:27 +0900

    site-packages

--- Pythonista3App/moveScript.py
+++ Pythonista3App/moveScript.py
@@ -11,16 +11,23 @@ def get_pythonista_modules_path(sys_paths: list[str]) -> str:
 
 
 def move_items(from_path: str, to_path: str) -> None:
-  shutil.copytree(from_path, to_path)
+  shutil.copytree(from_path, to_path, dirs_exist_ok=True)
 
 
 if __name__ == '__main__':
   pass2dir_name = 'modulesMaster'
 
   pythonista_modules_path = get_pythonista_modules_path(sys.path)
+  pythonista_modules_path = str(Path(pythonista_modules_path, '..').resolve())
 
   root = Path.cwd()
-  to_target = Path(root, pass2dir_name, parent_name)
+  #to_target = Path(root, pass2dir_name, parent_name)
+  to_target = Path(root, pass2dir_name, 'site-packages')
   target_path = str(to_target)
+  
+  
+  
+  #print(sys.path)
+  
   move_items(pythonista_modules_path, target_path)
 
