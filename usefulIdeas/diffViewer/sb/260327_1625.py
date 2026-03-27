9ae707af7cf6b1d9662dd21f8ac68a3f3ca87212
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-03-27 16:36:13 +0900

    init

---
+++ usefulIdeas/diffViewer/sb/260327_1625.py
@@ -0,0 +1,7 @@
+import difflib
+from pathlib import Path
+
+p1 = Path('/private/var/mobile/Containers/Shared/AppGroup/690154A9-9FC9-4B1D-8CB8-AB398AC342C4/File Provider Storage/Downloads/BeginningMetal/9 - Model IO/Final/RayBreak/RayBreak/Model.swift')
+p2 = Path('/private/var/mobile/Containers/Shared/AppGroup/690154A9-9FC9-4B1D-8CB8-AB398AC342C4/File Provider Storage/Downloads/BeginningMetal/9 - Model IO/Challenge/RayBreak/RayBreak/Model.swift')
+
+print(p1.exists())
